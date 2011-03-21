bcb5462f0eb9708f7fdf187a95134751dc252f29
Bernardo Heynemann <heynemann@gmail.com>
2011-03-21 11:20:48 -0300

    SIGHUP and SIGTERM support

--- server.py
+++ server.py
@@ -1,16 +1,51 @@
 
 import sys
+import signal
 
 import tornado.ioloop
+from tornado.httpserver import HTTPServer
 
 from thumbor.app import ThumborServiceApp
 
+port = 8888
+server = None
 
-def main(port):
+def __kill_server():
+    print 'stopping server...'
+    server.stop()
+    ioloop = tornado.ioloop.IOLoop.instance()
+    ioloop.stop()
+    ioloop._stopped = False
+
+def handle_sigterm(signum, frame):
+    __kill_server()
+    print "server stopped"
+
+def handle_sighup(signum, frame):
+    __kill_server()
+
+    print 'restarting server...'
+    main()
+
+def main():
+    global server
+
+    print 'starting server...'
+
+    if not signal.getsignal(signal.SIGHUP):
+        signal.signal(signal.SIGHUP, handle_sighup)
+    if not signal.getsignal(signal.SIGTERM):
+        signal.signal(signal.SIGTERM, handle_sigterm)
+
     application = ThumborServiceApp()
-    application.listen(port)
+
+    server = HTTPServer(application)
+    server.listen(port, '0.0.0.0')
+
+    print 'server started'
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8888
-    main(port)
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    main()
