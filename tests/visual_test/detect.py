3147d420ade179b597cbe9c4533a751bce27d2a5
Fabio M. Costa <fabiomcosta@gmail.com>
2011-04-14 00:39:13 -0300

    improving the visual_test detector to work like the current implementation of thumbor

--- tests/visual_test/detect.py
+++ tests/visual_test/detect.py
@@ -10,12 +10,21 @@
 
 import glob
 import os
+import tempfile
+import sys
 from os.path import dirname, abspath, join, basename, splitext, exists
 
 import cv
-from PIL import Image
+from PIL import Image as PILImage
+from tornado.options import define
+
+define('MAGICKWAND_PATH', [])
 
 __dirname = abspath(dirname(__file__))
+sys.path.insert(0, abspath(join(__dirname, '..', '..')))
+
+from thumbor.vendor.pythonmagickwand.image import Image
+
 
 cascade_files = (
     join('face_detector', 'haarcascade_frontalface_alt.xml'),
@@ -27,37 +36,38 @@ cascade_files = [abspath(join(__dirname, '..', '..', 'thumbor', 'detectors', cas
 
 images_path = glob.glob(abspath(join(__dirname, '..', 'fixtures', 'img', '*.*')))
 
-
 for cascade_file in cascade_files:
     loaded_cascade_file = cv.Load(cascade_file)
 
     for image_path in images_path:
-        image = Image.open(image_path)
-        size = image.size
-        image_header = cv.CreateImageHeader(size, cv.IPL_DEPTH_8U, 3)
-        cv.SetData(image_header, image.tostring())
-        grayscale = cv.CreateImage(size, 8, 1)
-        cv.CvtColor(image_header, grayscale, cv.CV_BGR2GRAY)
-        cv.EqualizeHist(grayscale, grayscale)
         
-        # opencv way of loading images
-        # grayscale = cv.LoadImageM(image_path, cv.CV_LOAD_IMAGE_GRAYSCALE)
-        # image_header = grayscale
-        
-        faces = cv.HaarDetectObjects(grayscale, loaded_cascade_file,
-            cv.CreateMemStorage(), 1.1, 3, cv.CV_HAAR_DO_CANNY_PRUNING, (30, 30))
+        with tempfile.NamedTemporaryFile() as temp_file:
+            file_name = temp_file.name
 
-        if faces:
-            for (left, top, width, height), neighbors in faces:
-                cv.Rectangle(
-                    image_header,
-                    (int(left), int(top)),
-                    (int(left + width), int(top + height)),
-                    255
-                )
-            cascade_file_name = splitext(basename(cascade_file))[0]
-            destination_folder = join(__dirname, 'img')
-            if not exists(destination_folder):
-                os.makedirs(destination_folder)
-            cv.SaveImage(join(destination_folder, '%s_%s' % (cascade_file_name, basename(image_path))), image_header)
-            print 'face detected on %s using %s' % (basename(image_path), basename(cascade_file))
+            #imagick
+            img = Image(image_path)
+            img.format = 'JPEG'
+            img.save(file_name)
+
+            #pil
+            #PILImage.open(image_path).convert('RGB').save(file_name, 'JPEG')
+
+            grayscale = cv.LoadImageM(file_name, cv.CV_LOAD_IMAGE_GRAYSCALE)
+
+            faces = cv.HaarDetectObjects(grayscale, loaded_cascade_file,
+                cv.CreateMemStorage(), 1.1, 3, cv.CV_HAAR_DO_CANNY_PRUNING, (30, 30))
+
+            if faces:
+                for (left, top, width, height), neighbors in faces:
+                    cv.Rectangle(
+                        grayscale,
+                        (int(left), int(top)),
+                        (int(left + width), int(top + height)),
+                        255
+                    )
+                cascade_file_name = splitext(basename(cascade_file))[0]
+                destination_folder = join(__dirname, 'img')
+                if not exists(destination_folder):
+                    os.makedirs(destination_folder)
+                cv.SaveImage(join(destination_folder, '%s_%s' % (cascade_file_name, basename(image_path))), grayscale)
+                print 'face detected on %s using %s' % (basename(image_path), basename(cascade_file))
