60ebe3e8b6c041445ce717b5282b2eaf6e46e513
Bernardo Heynemann <heynemann@gmail.com>
2011-04-16 14:13:07 -0300

    Redis storage

--- thumbor/storages/redis_storage.py
+++ thumbor/storages/redis_storage.py
@@ -26,9 +26,26 @@ class Storage(BaseStorage):
                                    host=options.REDIS_STORAGE_SERVER_HOST,
                                    db=options.REDIS_STORAGE_SERVER_DB)
 
+    def __key_for(self, url):
+        return 'crypto-%s' % url
+
     def put(self, path, bytes):
         self.storage.set(path, bytes)
         self.storage.expireat(path, datetime.now() + timedelta(seconds=options.STORAGE_EXPIRATION_SECONDS))
+
+        if options.STORES_CRYPTO_KEY_FOR_EACH_IMAGE:
+
+            if not options.SECURITY_KEY:
+                raise RuntimeError("STORES_CRYPTO_KEY_FOR_EACH_IMAGE can't be True if no SECURITY_KEY specified")
+
+            self.storage.set(self.__key_for(path), options.SECURITY_KEY)
+
+    def get_crypto(self, path):
+        crypto = self.storage.get(self.__key_for(path))
+
+        if not crypto:
+            return None
+        return crypto
 
     def get(self, path):
         return self.storage.get(path)
