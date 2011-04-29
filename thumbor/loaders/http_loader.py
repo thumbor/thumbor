231152d25123add0a35b1e42fce3f6b32d9fe41f
Bernardo Heynemann <heynemann@gmail.com>
2011-04-29 19:22:37 -0300

    Load async

--- thumbor/loaders/http_loader.py
+++ thumbor/loaders/http_loader.py
@@ -4,20 +4,23 @@
 # thumbor imaging service
 # https://github.com/globocom/thumbor/wiki
 
-# Licensed under the MIT license: 
+# Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
 
 import re
 from urlparse import urlparse
-import urllib2
 
+import eventlet
+from eventlet.green import urllib2
 from tornado.httpclient import HTTPClient, HTTPRequest
 from tornado.options import define, options
 
 define('ALLOWED_SOURCES', default=[], multiple=True)
 define('MAX_SOURCE_SIZE', type=int, default=0)
 define('REQUEST_TIMEOUT_SECONDS', type=int, default=120)
+
+pool = eventlet.GreenPool()
 
 def __normalize_url(url):
     return url if url.startswith('http') else 'http://%s' % url
@@ -34,21 +37,36 @@ def validate(url):
     return False
 
 def verify_size(url, max_size):
-    usock = urllib2.urlopen(__normalize_url(url))
-    size =  usock.info().get('Content-Length')
-    if size is None:
-        size = 0
-    return (float(size) / 1024) <= max_size
+    def verify(options):
+        source, size = options
 
-def load(url):
+        usock = urllib2.urlopen(source)
+        actual = usock.info().get('Content-Length')
+        if actual is None:
+            actual = 0
+        return (float(actual) / 1024) <= size
+
+    for verified in pool.imap(verify, [(__normalize_url(url), max_size)]):
+        return verified
+
+def load(url, callback):
     if options.MAX_SOURCE_SIZE and not verify_size(url, options.MAX_SOURCE_SIZE):
         return None
 
-    url = __normalize_url(url)
-    http = HTTPClient()
-    request = HTTPRequest(url=url, request_timeout=options.REQUEST_TIMEOUT_SECONDS)
-    response = http.fetch(request)
-    if response.code == 200:
-        return response.body
-    else:
-        return None
+    def actual_load(actual_url):
+        usock = urllib2.urlopen(actual_url, timeout=options.REQUEST_TIMEOUT_SECONDS)
+
+        try:
+            return usock.read()
+        except urllib2.HTTPError:
+            return None
+
+    func = pool.spawn(actual_load, url)
+
+    def return_contents(gt, url, callback):
+        contents = gt.wait()
+
+        callback(contents)
+
+    func.link(return_contents, url, callback)
+
--- vows/http_loader_vows.py
+++ vows/http_loader_vows.py
@@ -13,9 +13,13 @@ from os.path import abspath, join, dirname
 from tornado.options import options, parse_config_file
 from pyvows import Vows, expect
 
-from thumbor.loaders.http_loader import load
+from thumbor.loaders.http_loader import load, verify_size
 
-config_file_path = lambda filename: abspath(join(dirname(__file__), 'fixtures', filename))
+fixture_for = lambda filename: abspath(join(dirname(__file__), 'fixtures', filename))
+
+@Vows.assertion
+def to_match_contents_of(topic, filepath):
+    assert file(filepath).read() == topic, "Expected body of %s to match topic, but it didn't" % filepath
 
 @Vows.batch
 class HttpLoader(Vows.Context):
@@ -51,7 +55,7 @@ class HttpLoader(Vows.Context):
 
             class ThenSpecified(Vows.Context):
                 def topic(self):
-                    parse_config_file(config_file_path('http_loader_options.py'))
+                    parse_config_file(fixture_for('http_loader_options.py'))
 
                 class AllowedSources(Vows.Context):
                     def topic(self):
@@ -80,6 +84,28 @@ class HttpLoader(Vows.Context):
                     def should_be_three_minutes(self, topic):
                         expect(topic).to_equal(180)
 
-    #class WhenInstanceCreated(Vows.Context):
-        #def topic(self):
-            #return HttpLoader()
+    class LoadAndVerifyImage(Vows.Context):
+        def topic(self):
+            return 'http://s.glbimg.com/jo/g1/f/original/2011/04/29/conselheira_tutelar.jpg'
+
+        class Verify(Vows.Context):
+            class WhenInMaxSize(Vows.Context):
+                def topic(self, url):
+                    return verify_size(url, 50 * 1024)
+
+                def should_return_true_to_fifty_megs(self, topic):
+                    expect(topic).to_be_true()
+
+            class WhenOutOfMaxSize(Vows.Context):
+                def topic(self, url):
+                    return verify_size(url, 1)
+
+                def should_return_false_to_one_kb(self, topic):
+                    expect(topic).to_be_false()
+
+        class Load(Vows.Context):
+            def should_equal_regular_http_get(self, topic):
+                def callback(contents):
+                    expect(contents).to_match_contents_of(fixture_for('conselheira_tutelar.jpg'))
+
+                load(topic, callback)
