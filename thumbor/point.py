cb764f4d99e933e42a833a222a91f153f3558aa3
Bernardo Heynemann <heynemann@gmail.com>
2011-07-29 15:21:04 -0300

    Point

--- thumbor/point.py
+++ thumbor/point.py
@@ -27,7 +27,7 @@ class FocalPoint(object):
 
     @classmethod
     def from_dict(cls, values):
-        return cls(values['x'], values['y'], values['z'])
+        return cls(int(values['x']), int(values['y']), int(values['z']))
 
     def __init__(self, x, y, weight=1.0):
         self.x = x
