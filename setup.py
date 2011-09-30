a836be25b04f99d8523ae68785e5b4827c45762c
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2011-09-30 11:36:24 -0300

    Improving setup.py to keep working even after extension compiling errors.

--- setup.py
+++ setup.py
@@ -11,11 +11,26 @@
 from setuptools import setup, Extension
 from thumbor import __version__
 
-setup(
-    name = 'thumbor',
-    version = __version__,
-    description = "thumbor is an open-source photo thumbnail service by globo.com",
-    long_description = """
+import glob
+
+def filter_extension_module(name):
+    return Extension(
+        'thumbor.ext.filters.%s' % name,
+        ['thumbor/ext/filters/%s.c' % name],
+        libraries = ['m'],
+        depends = ['thumbor/ext/filters/filter.h', 'setup.py'],
+        extra_compile_args = ['-ansi', '-Wall', '-Wextra', '-Werror', '-Wno-unused-parameter'])
+
+def gather_filter_extensions():
+    files = glob.glob('thumbor/ext/filters/*.c')
+    return [filter_extension_module(f[0:-2].split('/')[-1]) for f in files]
+
+def run_setup(extension_modules = []):
+  setup(
+      name = 'thumbor',
+      version = __version__,
+      description = "thumbor is an open-source photo thumbnail service by globo.com",
+      long_description = """
 Thumbor is a smart imaging service. It enables on-demand crop, resizing and flipping of images.
 
 It also features a VERY smart detection of important points in the image for better cropping and resizing, using state-of-the-art face and feature detection algorithms (more on that in Detection Algorithms).
@@ -23,50 +38,50 @@ It also features a VERY smart detection of important points in the image for bet
 Using thumbor is very easy (after it is running). All you have to do is access it using an url for an image, like this:
 
 http://<thumbor-server>/300x200/smart/s.glbimg.com/et/bb/f/original/2011/03/24/VN0JiwzmOw0b0lg.jpg
-""",    
-    keywords = 'imaging face detection feature thumbnail imagemagick pil opencv',
-    author = 'globo.com',
-    author_email = 'timehome@corp.globo.com',
-    url = 'https://github.com/globocom/thumbor/wiki',
-    license = 'MIT',
-    classifiers = ['Development Status :: 4 - Beta',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Natural Language :: English',
-                   'Operating System :: MacOS',
-                   'Operating System :: POSIX :: Linux',
-                   'Programming Language :: Python :: 2.6',
-                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-                   'Topic :: Multimedia :: Graphics :: Presentation'
-    ],
-    packages = ['thumbor'],
-    package_dir = {"thumbor": "thumbor"},
-    include_package_data = True,
-    package_data = {
-        '': ['*.xml'],
-    },
+""",
+      keywords = 'imaging face detection feature thumbnail imagemagick pil opencv',
+      author = 'globo.com',
+      author_email = 'timehome@corp.globo.com',
+      url = 'https://github.com/globocom/thumbor/wiki',
+      license = 'MIT',
+      classifiers = ['Development Status :: 4 - Beta',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: MIT License',
+                     'Natural Language :: English',
+                     'Operating System :: MacOS',
+                     'Operating System :: POSIX :: Linux',
+                     'Programming Language :: Python :: 2.6',
+                     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                     'Topic :: Multimedia :: Graphics :: Presentation'
+      ],
+      packages = ['thumbor'],
+      package_dir = {"thumbor": "thumbor"},
+      include_package_data = True,
+      package_data = {
+          '': ['*.xml'],
+      },
 
-    install_requires=[
-        "tornado==1.2.1",
-        "redis",
-        "pyCrypto",
-        "pycurl",
-        "pillow"
-    ],
+      install_requires=[
+          "tornado==1.2.1",
+          "redis",
+          "pyCrypto",
+          "pycurl",
+          "pillow"
+      ],
 
-    entry_points = {
-        'console_scripts': [
-            'thumbor = thumbor.server:main',
-            'thumbor-url = thumbor.url_composer:main'
-        ],
-    },
+      entry_points = {
+          'console_scripts': [
+              'thumbor = thumbor.server:main',
+              'thumbor-url = thumbor.url_composer:main'
+          ],
+      },
 
-    ext_modules = [
-      Extension('thumbor.ext.filters._brightness', ['thumbor/ext/filters/_brightness.c']),
-      Extension('thumbor.ext.filters._contrast', ['thumbor/ext/filters/_contrast.c']),
-      Extension('thumbor.ext.filters._rgb', ['thumbor/ext/filters/_rgb.c']),
-      Extension('thumbor.ext.filters._round_corner', ['thumbor/ext/filters/_round_corner.c'])
-    ]
-)
+      ext_modules = extension_modules
+  )
 
-
+try:
+    run_setup(gather_filter_extensions())
+except SystemExit as exit:
+    print "\n\n*******************************************************************"
+    print "Couldn't build one or more native extensions, skipping compilation.\n\n"
+    run_setup()
--- thumbor/ext/filters/filter.h
+++ thumbor/ext/filters/filter.h
@@ -3,14 +3,16 @@
 
 #include <Python.h>
 
+#include <math.h>
+
 #define ADJUST_COLOR(c) ((c > 255) ? 255 : ((c < 0) ? 0 : c))
 
-#define FILTER_MODULE(NAME, DOC) static PyMethodDef NAME ## _methods[] = { \
-                                {"apply", NAME ## _apply, METH_VARARGS, DOC}, \
-                                {NULL, NULL} \
-                            }; \
-                            PyMODINIT_FUNC init ## NAME(void) { \
-                                Py_InitModule3(#NAME, NAME ## _methods, #NAME " native module"); \
-                            }
+#define FILTER_MODULE(NAME, DOC)    static PyMethodDef NAME ## _methods[] = { \
+                                        {"apply", NAME ## _apply, METH_VARARGS, DOC}, \
+                                        {0, 0, 0, 0} \
+                                    }; \
+                                    PyMODINIT_FUNC init ## NAME(void) { \
+                                        Py_InitModule3(#NAME, NAME ## _methods, #NAME " native module"); \
+                                    }
 
 #endif
