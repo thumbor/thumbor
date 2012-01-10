ce5465e18afeb03ae4ceaafb10364c1a4b3d20e5
Bernardo Heynemann <heynemann@gmail.com>
2012-01-10 11:39:04 -0200

    Docs and begginnings

--- thumbor/context.py
+++ thumbor/context.py
@@ -9,5 +9,30 @@
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
 
 class Context:
-    def __init__(self):
-        pass
+    '''
+    Class responsible for containing:
+    * Server Configuration Parameters (port, ip, key, etc);
+    * Configurations read from config file (or defaults);
+    * Importer with imported modules (engine, filters, detectors, etc);
+    * Request Parameters (width, height, smart, meta, etc).
+
+    Each instance of this class MUST be unique per request. This class should not be cached in the server.
+    '''
+
+    def __init__(self, server=None, config=None, importer=None, request=None):
+        self.server = server
+        self.config = config
+        self.modules = ContextImporter(importer)
+        self.request = request
+
+class ServerParameters:
+    pass
+
+class RequestParameters:
+    pass
+
+class ContextImporter:
+    def __init__(self, importer):
+        self.importer = importer
+
+
