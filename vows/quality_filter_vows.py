53c024f5d8c78bca554076243b41dffd27399395
Bernardo Heynemann <heynemann@gmail.com>
2012-01-11 18:15:34 -0200

    More test fixes

--- vows/quality_filter_vows.py
+++ vows/quality_filter_vows.py
@@ -12,16 +12,23 @@ from pyvows import Vows, expect
 
 from thumbor.filters import create_instances, compile_filters
 from thumbor.filters.quality import Filter
+from thumbor.context import Context, RequestParameters
+from thumbor.config import Config
 
 @Vows.batch
 class QualityFilterVows(Vows.Context):
     def topic(self):
-        ctx = { 'quality': 100, 'engine': None }
+        conf = Config()
+        req = RequestParameters(quality=100)
+        ctx = Context(None, conf, None)
+        ctx.request = req
+
         filters = [Filter]
         compile_filters(filters)
         filter_instances = create_instances(ctx, filters, "quality(10)")
+
         filter_instances[0].run_filter()
-        return ctx['quality']
+        return ctx.request.quality
 
     def should_equal_10(self, topic):
         expect(topic).to_equal(10)
