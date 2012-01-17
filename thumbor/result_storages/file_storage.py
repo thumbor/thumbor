39ee2b52a402f68cbdcd9baf3282fa872f18ee2f
Bernardo Heynemann <heynemann@gmail.com>
2012-01-17 19:27:55 -0200

    Results storage improved

--- thumbor/result_storages/file_storage.py
+++ thumbor/result_storages/file_storage.py
@@ -23,7 +23,7 @@ class Storage(BaseStorage):
             os.makedirs(path)
 
     def put(self, bytes):
-        file_abspath = self.__normalize_path(self.context.request.url)
+        file_abspath = self.normalize_path(self.context.request.url)
         file_dir_abspath = dirname(file_abspath)
         logger.debug("[RESULT_STORAGE] putting at %s (%s)" % (file_abspath, file_dir_abspath))
 
@@ -34,19 +34,24 @@ class Storage(BaseStorage):
 
     def get(self):
         path = self.context.request.url
-        file_abspath = self.__normalize_path(path)
+        file_abspath = self.normalize_path(path)
         logger.debug("[RESULT_STORAGE] getting from %s" % file_abspath)
 
-        if not exists(file_abspath) or self.__is_expired(file_abspath):
+        if not exists(file_abspath) or self.is_expired(file_abspath):
             logger.debug("[RESULT_STORAGE] image not found at %s" % file_abspath)
             return None
         return open(file_abspath, 'r').read()
 
-    def __normalize_path(self, path):
+    def normalize_path(self, path):
         path = join(self.context.config.RESULT_STORAGE_FILE_STORAGE_ROOT_PATH.rstrip('/'), path.lstrip('/'))
         path = path.replace('http://', '')
         return path
 
-    def __is_expired(self, path):
+    def is_expired(self, path):
+        expire_in_seconds = self.context.config.get('RESULT_STORAGE_EXPIRATION_SECONDS', None)
+
+        if expire_in_seconds is None or expire_in_seconds == 0:
+            return False
+
         timediff = datetime.now() - datetime.fromtimestamp(getmtime(path))
-        return timediff.seconds > self.context.config.RESULT_STORAGE_EXPIRATION_SECONDS
+        return timediff.seconds > expire_in_seconds
