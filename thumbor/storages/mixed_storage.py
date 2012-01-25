7b1ac04decc933da03126a1ded0a8723a5370a9d
Bernardo Heynemann <heynemann@gmail.com>
2012-01-25 14:15:08 -0200

    Mixed storage refactor

--- thumbor/storages/mixed_storage.py
+++ thumbor/storages/mixed_storage.py
@@ -4,7 +4,7 @@
 # thumbor imaging service
 # https://github.com/globocom/thumbor/wiki
 
-# Licensed under the MIT license: 
+# Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
 
@@ -15,45 +15,55 @@ class Storage(BaseStorage):
         BaseStorage.__init__(self, context)
 
         self.file_storage = file_storage
+        self._init_file_storage()
+
         self.crypto_storage = crypto_storage
+        self._init_crypto_storage()
+
         self.detector_storage = detector_storage
+        self._init_detector_storage()
 
     def _init_file_storage(self):
         if self.file_storage is None:
-            self.context.modules.importer.import_item(config_key='file_storage', item_value=self.context.config.MIXED_STORAGE_FILE_STORAGE, class_name='Storage')
+            self.context.modules.importer.import_item(
+                config_key='file_storage',
+                item_value=self.context.config.MIXED_STORAGE_FILE_STORAGE,
+                class_name='Storage'
+            )
             self.file_storage = self.context.modules.file_storage = self.context.modules.importer.file_storage(self.context)
 
     def _init_crypto_storage(self):
         if self.crypto_storage is None:
-            self.context.modules.importer.import_item(config_key='crypto_storage', item_value=self.context.config.MIXED_STORAGE_CRYPTO_STORAGE, class_name='Storage')
+            self.context.modules.importer.import_item(
+                config_key='crypto_storage',
+                item_value=self.context.config.MIXED_STORAGE_CRYPTO_STORAGE,
+                class_name='Storage'
+            )
             self.crypto_storage = self.context.modules.crypto_storage = self.context.modules.importer.crypto_storage(self.context)
 
     def _init_detector_storage(self):
         if self.detector_storage is None:
-            self.context.modules.importer.import_item(config_key='detector_storage', item_value=self.context.config.MIXED_STORAGE_DETECTOR_STORAGE, class_name='Storage')
+            self.context.modules.importer.import_item(
+                config_key='detector_storage',
+                item_value=self.context.config.MIXED_STORAGE_DETECTOR_STORAGE,
+                class_name='Storage'
+            )
             self.detector_storage = self.context.modules.detector_storage = self.context.modules.importer.detector_storage(self.context)
 
     def put(self, path, bytes):
-        self._init_file_storage()
         self.file_storage.put(path, bytes)
 
     def put_detector_data(self, path, data):
-        self._init_detector_storage()
         self.detector_storage.put_detector_data(path, data)
 
     def put_crypto(self, path):
-        self._init_crypto_storage()
         self.crypto_storage.put_crypto(path)
 
     def get_crypto(self, path):
-        self._init_crypto_storage()
         return self.crypto_storage.get_crypto(path)
 
     def get_detector_data(self, path):
-        self._init_detector_storage()
         return self.detector_storage.get_detector_data(path)
 
     def get(self, path):
-        self._init_file_storage()
         return self.file_storage.get(path)
-
