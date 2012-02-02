ed7df55b649fa2a590d132eb2aadd5ca17b98f0a
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2012-02-02 14:19:34 -0200

    Errors during queued detectors shouldn't prevent the image from being delivered.

--- thumbor/detectors/queued_detector/__init__.py
+++ thumbor/detectors/queued_detector/__init__.py
@@ -1,18 +1,27 @@
+import traceback
+
 from pyremotecv import PyRemoteCV
+from redis import RedisError
 
 from thumbor.detectors import BaseDetector
+from thumbor.utils import logger
 
 class QueuedDetector(BaseDetector):
     pyremotecv = None
 
     def detect(self, callback):
-        if not self.pyremotecv:
-            self.pyremotecv = PyRemoteCV(host=self.context.config.REDIS_QUEUE_SERVER_HOST,
-                                         port=self.context.config.REDIS_QUEUE_SERVER_PORT,
-                                         db=self.context.config.REDIS_QUEUE_SERVER_DB,
-                                         password=self.context.config.REDIS_QUEUE_SERVER_PASSWORD)
-        self.pyremotecv.async_detect('remotecv.pyres_tasks.DetectTask', 'Detect',
-                args=[self.detection_type, self.context.request.image_url],
-                key=self.context.request.image_url)
         self.context.prevent_result_storage = True
-        callback([])
+        try:
+            if not self.pyremotecv:
+                self.pyremotecv = PyRemoteCV(host=self.context.config.REDIS_QUEUE_SERVER_HOST,
+                                             port=self.context.config.REDIS_QUEUE_SERVER_PORT,
+                                             db=self.context.config.REDIS_QUEUE_SERVER_DB,
+                                             password=self.context.config.REDIS_QUEUE_SERVER_PASSWORD)
+
+            self.pyremotecv.async_detect('remotecv.pyres_tasks.DetectTask', 'Detect',
+                    args=[self.detection_type, self.context.request.image_url],
+                    key=self.context.request.image_url)
+        except RedisError:
+            logger.error(traceback.format_exc())
+        finally:
+            callback([])
