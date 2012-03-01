b09fa406b6f4f745d33d49a92aa2a44ffb86066f
Damien Hardy <dhardy@figarocms.fr>
2012-03-01 13:27:35 +0100

    improve fill() filter with 0 requested size

--- thumbor/filters/fill.py
+++ thumbor/filters/fill.py
@@ -9,15 +9,14 @@
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
 
 from thumbor.filters import BaseFilter
-from PIL import Image
 
 class Filter(BaseFilter):
     regex = r'(?:fill\((?P<value>[\w]+)\))'
 
     def run_filter(self):
         self.fill_engine = self.engine.__class__(self.context)
-        bx = self.context.request.width
-        by = self.context.request.height
+        bx = self.context.request.width if self.context.request.width != 0 else self.engine.image.size[0]
+        by = self.context.request.height if self.context.request.height != 0 else self.engine.image.size[1]
         (ix, iy) = self.engine.image.size
         try:
             self.fill_engine.image = self.fill_engine.gen_image((bx,by),self.params['value'])
@@ -26,6 +25,7 @@ class Filter(BaseFilter):
 
         px = ( bx - ix )/2 #top left
         py = ( by - iy )/2
-	
+
+
         self.fill_engine.paste(self.engine, (px, py))
         self.engine.image = self.fill_engine.image
