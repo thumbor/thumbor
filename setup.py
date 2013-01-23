ebaf8f32cbed82120e611f7822b3f3b3e9785606
dhardy92 <dhardy@viadeoteam.com>
2013-01-23 14:51:49 +0100

    as ubuntu 12.10 comes with python-crypto 2.6-2, thumbor does not want starting

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ http://<thumbor-server>/300x200/smart/s.glbimg.com/et/bb/f/original/2011/03/24/V
 
       install_requires=[
           "tornado>=2.1.1,<2.2.0",
-          "pyCrypto>=2.4.1,<2.5.0",
+          "pyCrypto>=2.4.1",
           "pycurl>=7.19.0,<7.20.0",
           "Pillow>=1.7.5,<1.8.0",
           "derpconf>=0.2.0",
