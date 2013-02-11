66b8500cfa895574df686cfd8b3d52e6e5c143f8
Fabio M. Costa <fabiomcosta@gmail.com>
2013-02-10 23:40:58 -0500

    The color auto detection is now made in c and doesnt use opencv. Got a speed bump of around 100%.
    Current ab results:
    
    ab -n 100 -c 4 "http://127.0.0.1:8888/unsafe/fit-in/500x500/filters:fill(auto)/http://www.hdwallpapers.in/walls/abstract_color_background_picture_8016-wide.jpg"
    This is ApacheBench, Version 2.3 <$Revision: 655654 $>
    Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/
    Licensed to The Apache Software Foundation, http://www.apache.org/
    
    Benchmarking 127.0.0.1 (be patient)...Send request failed!
    Send request failed!
    ..done
    
    Server Software:        Thumbor/3.7.1
    Server Hostname:        127.0.0.1
    Server Port:            8888
    
    Document Path:          /unsafe/fit-in/500x500/filters:fill(auto)/http://www.hdwallpapers.in/walls/abstract_color_background_picture_8016-wide.jpg
    Document Length:        15434 bytes
    
    Concurrency Level:      4
    Time taken for tests:   25.531 seconds
    Complete requests:      100
    Failed requests:        1
    (Connect: 0, Receive: 0, Length: 1, Exceptions: 0)
    Write errors:           2
    Total transferred:      1542088 bytes
    HTML transferred:       1520506 bytes
    Requests per second:    3.92 [#/sec] (mean)
    Time per request:       1021.230 [ms] (mean)
    Time per request:       255.307 [ms] (mean, across all concurrent requests)
    Transfer rate:          58.99 [Kbytes/sec] received
    
    Connection Times (ms)
    min  mean[+/-sd] median   max
    Connect:        0  280 437.2     17    1049
    Processing:     0  726 460.1    988    1357
    Waiting:        0  725 459.8    987    1357
    Total:        258 1006 133.1   1014    1357
    
    Percentage of the requests served within a certain time (ms)
    50%   1014
    66%   1028
    75%   1033
    80%   1036
    90%   1040
    95%   1043
    98%   1357
    99%   1357
    100%   1357 (longest request)
    
    New C implemented auto detection results:
    
    ab -n 100 -c 4 "http://127.0.0.1:8888/unsafe/fit-in/500x500/filters:fill(auto)/http://www.hdwallpapers.in/walls/abstract_color_background_picture_8016-wide.jpg"
    This is ApacheBench, Version 2.3 <$Revision: 655654 $>
    Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/
    Licensed to The Apache Software Foundation, http://www.apache.org/
    
    Benchmarking 127.0.0.1 (be patient).....done
    
    Server Software:        Thumbor/3.7.1
    Server Hostname:        127.0.0.1
    Server Port:            8888
    
    Document Path:          /unsafe/fit-in/500x500/filters:fill(auto)/http://www.hdwallpapers.in/walls/abstract_color_background_picture_8016-wide.jpg
    Document Length:        15434 bytes
    
    Concurrency Level:      4
    Time taken for tests:   13.589 seconds
    Complete requests:      100
    Failed requests:        0
    Write errors:           0
    Total transferred:      1565200 bytes
    HTML transferred:       1543400 bytes
    Requests per second:    7.36 [#/sec] (mean)
    Time per request:       543.559 [ms] (mean)
    Time per request:       135.890 [ms] (mean, across all concurrent requests)
    Transfer rate:          112.48 [Kbytes/sec] received
    
    Connection Times (ms)
    min  mean[+/-sd] median   max
    Connect:        0    0   0.0      0       0
    Processing:   152  535  48.3    541     566
    Waiting:      152  535  48.3    541     566
    Total:        153  535  48.3    541     566
    
    Percentage of the requests served within a certain time (ms)
    50%    541
    66%    543
    75%    544
    80%    547
    90%    553
    95%    556
    98%    566
    99%    566
    100%    566 (longest request)

---
+++ thumbor/ext/filters/_fill.c
@@ -0,0 +1,49 @@
+#include "filter.h"
+
+static PyObject*
+_fill_apply(PyObject *self, PyObject *args)
+{
+    PyObject *buffer = NULL, *image_mode = NULL;
+
+    if (!PyArg_UnpackTuple(args, "apply", 2, 2, &image_mode, &buffer)) {
+        return NULL;
+    }
+
+    char *image_mode_str = PyString_AsString(image_mode);
+    Py_ssize_t size = PyString_Size(buffer);
+    unsigned char *ptr = (unsigned char *) PyString_AsString(buffer);
+
+    int num_bytes = bytes_per_pixel(image_mode_str);
+    int r_idx = rgb_order(image_mode_str, 'R'),
+        g_idx = rgb_order(image_mode_str, 'G'),
+        b_idx = rgb_order(image_mode_str, 'B');
+
+    int i = 0, image_area = (size / num_bytes);
+    unsigned long r = 0, g = 0, b = 0;
+
+    size -= num_bytes;
+    for (; i <= size; i += num_bytes) {
+        r += ptr[i + r_idx];
+        g += ptr[i + g_idx];
+        b += ptr[i + b_idx];
+    }
+
+    r /= image_area;
+    g /= image_area;
+    b /= image_area;
+
+    // TODO
+    // I tried making this function return a hex color string as "23abdd"
+    // but PyString_FromFormat couldnt zero pad the numbers
+    // example PyString_FromFormat("%02x%02x%02x", 3, 1, 11) returns "31b"
+    // the correct would be "03010b"
+    // http://docs.python.org/2/c-api/string.html#PyString_FromFormat
+
+    return Py_BuildValue("(lll)", r, g, b);
+}
+
+FILTER_MODULE(_fill,
+    "apply(delta, buffer) -> string\n"
+    "Auto detects the color to fill the image with, assuming "
+    "'buffer' as a Python string. Returns a tuple in the format (r, g, b)."
+)
