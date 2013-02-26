596a96d09fe13098f148bbc9bb8cb49421f65a30
Fabio M. Costa <fabiomcosta@gmail.com>
2013-02-26 17:44:04 -0500

    making sure that the necessary api is respected

--- thumbor/result_storages/__init__.py
+++ thumbor/result_storages/__init__.py
@@ -4,10 +4,17 @@
 # thumbor imaging service
 # https://github.com/globocom/thumbor/wiki
 
-# Licensed under the MIT license: 
+# Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
+
 
 class BaseStorage(object):
     def __init__(self, context):
         self.context = context
+
+    def put(self, bytes):
+        raise NotImplementedError
+
+    def get(self):
+        raise NotImplementedError
