47024e12f41a18fc0c99b8213a82c3b3456210a8
William King <will@upout.com>
2013-03-11 22:56:26 -0700

    Fix s3 /// path bug and add hashed result storage

--- thumbor/loaders/s3_loader.py
+++ thumbor/loaders/s3_loader.py
@@ -7,45 +7,45 @@ from boto.s3.key import Key
 connection = None
 
 def _get_bucket(url):
-	#first item in URL
-	arr = url.lstrip('/').split('/')
-	return arr[0],"/".join(arr[1:])
+    #first item in URL
+    arr = url.lstrip('/').split('/',1)
+    return arr[0],"/"+arr[1]
 
 def _validate_bucket(context,bucket):
-	if not context.config.S3_ALLOWED_BUCKETS:
-		return True
+    if not context.config.S3_ALLOWED_BUCKETS:
+        return True
 
-	for allowed in context.config.S3_ALLOWED_BUCKETS:
-		#s3 is case sensitive
-		if allowed == bucket:
-			return True
+    for allowed in context.config.S3_ALLOWED_BUCKETS:
+        #s3 is case sensitive
+        if allowed == bucket:
+            return True
 
-	return False
+    return False
 
 def load(context, url, callback):
-	
-	if context.config.S3_LOADER_BUCKET:
-		bucket = context.config.S3_LOADER_BUCKET
-	else:
-		bucket,url = _get_bucket(url)
-		if not _validate_bucket(context,bucket):
-			return callback(None)
+    
+    if context.config.S3_LOADER_BUCKET:
+        bucket = context.config.S3_LOADER_BUCKET
+    else:
+        bucket,url = _get_bucket(url)
+        if not _validate_bucket(context,bucket):
+            return callback(None)
 
-	conn = connection
-	if conn is None:
-		#Store connection not bucket
-		conn = S3Connection(
-			context.config.AWS_ACCESS_KEY,
-			context.config.AWS_SECRET_KEY
-		)
+    conn = connection
+    if conn is None:
+        #Store connection not bucket
+        conn = S3Connection(
+            context.config.AWS_ACCESS_KEY,
+            context.config.AWS_SECRET_KEY
+        )
 
-	bucketLoader = Bucket(
-		connection=conn,
-		name=bucket
-	)
+    bucketLoader = Bucket(
+        connection=conn,
+        name=bucket
+    )
 
-	file_key = bucketLoader.get_key(url)
-	if not file_key:
-		return callback(None)
+    file_key = bucketLoader.get_key(url)
+    if not file_key:
+        return callback(None)
 
-	return callback(file_key.read())
+    return callback(file_key.read())
