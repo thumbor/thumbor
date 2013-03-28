50ca338e7c680332c23b347ea2e17a3873ff66a3
Bernardo Heynemann <heynemann@gmail.com>
2013-03-27 22:35:57 -0300

    Release 3.8.0, fixes 175

--- thumbor/__init__.py
+++ thumbor/__init__.py
@@ -4,10 +4,10 @@
 # thumbor imaging service
 # https://github.com/globocom/thumbor/wiki
 
-# Licensed under the MIT license: 
+# Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
 
 '''This is the main module in thumbor'''
 
-__version__ = "3.7.3"
+__version__ = "3.8.0"
