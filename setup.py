f782e71847912d36cf93e06d92c5539e9d771526
Bernardo Heynemann <heynemann@gmail.com>
2013-10-30 20:36:50 -0200

    Release 3.14.7

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ http://<thumbor-server>/300x200/smart/s.glbimg.com/et/bb/f/original/2011/03/24/V
         },
 
         install_requires=[
-            "tornado>=2.1.1,<3.1.0",
+            "tornado>=2.1.1,<3.2.0",
             "pyCrypto>=2.1.0",
             "pycurl>=7.19.0,<7.20.0",
             "Pillow>=2.1.0,<2.2.0",
