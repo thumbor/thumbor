8a71225183b2ffd516c826cee2d961cf2f3f798b
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2014-02-04 01:04:36 -0200

    Bumping Pillow dependency, fixes #248

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ http://<thumbor-server>/300x200/smart/s.glbimg.com/et/bb/f/original/2011/03/24/V
             "tornado>=2.1.1,<3.2.0",
             "pyCrypto>=2.1.0",
             "pycurl>=7.19.0,<7.20.0",
-            "Pillow>=2.1.0,<2.2.0",
+            "Pillow>=2.1.0,<2.4.0",
             "derpconf>=0.2.0",
             "python-magic>=0.4.3",
             "pexif==0.13",
