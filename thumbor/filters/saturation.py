a84f6cb15323b48887fcf9428c3a5d5faa05c386
Clif Reeder <clif@voxmedia.com>
2014-11-17 16:31:35 -0500

    Add saturation filter

---
+++ thumbor/filters/saturation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/globocom/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com timehome@corp.globo.com
+
+from thumbor.filters import BaseFilter, filter_method
+from thumbor.ext.filters import _saturation
+
+class Filter(BaseFilter):
+
+    @filter_method(BaseFilter.DecimalNumber)
+    def saturation(self, change):
+        mode, data = self.engine.image_data_as_rgb()
+        imgdata = _saturation.apply(mode, change, data)
+        self.engine.set_image_data(imgdata)
