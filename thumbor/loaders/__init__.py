d1b72faa476c7e7d9ec3f0ab1cba3e35caa45d5c
Martin Samson <martin@workshopx.com>
2015-09-11 11:21:17 -0400

    Example of LoaderResult object usage with file_storage.

--- thumbor/loaders/__init__.py
+++ thumbor/loaders/__init__.py
@@ -7,3 +7,28 @@
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
+
+
+class LoaderResult(object):
+
+    ERROR_NOT_FOUND = 'not_found'
+    ERROR_UPSTREAM = 'upstream'
+
+    def __init__(self, buffer=None, successful=True, error=None, metadata=dict()):
+        '''
+        :param buffer: The media buffer
+
+        :param successful: True when the media has been loaded.
+        :type successful: bool
+
+        :param error: Error code
+        :type error: str
+
+        :param metadata: Dictionary of metadata about the buffer
+        :type metadata: dict
+        '''
+
+        self.buffer = buffer
+        self.successful = successful
+        self.error = error
+        self.metadata = metadata
--- thumbor/loaders/file_loader.py
+++ thumbor/loaders/file_loader.py
@@ -8,6 +8,9 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
 
+from . import LoaderResult
+from datetime import datetime
+from os import fstat
 from os.path import join, exists, abspath
 from urllib import unquote
 from tornado.concurrent import return_future
@@ -19,8 +22,22 @@ def load(context, path, callback):
     file_path = abspath(file_path)
     inside_root_path = file_path.startswith(context.config.FILE_LOADER_ROOT_PATH)
 
+    result = LoaderResult()
+
     if inside_root_path and exists(file_path):
+
         with open(file_path, 'r') as f:
-            callback(f.read())
+            stats = fstat(f.fileno())
+
+            result.successful = True
+            result.buffer = f.read()
+
+            result.metadata.update(
+                size=stats.st_size,
+                updated_at=datetime.datetime.utcfromtimestamp(stats.st_mtime)
+            )
     else:
-        callback(None)
+        result.error = LoaderResult.ERROR_NOT_FOUND
+        result.successful = False
+
+    callback(result)
