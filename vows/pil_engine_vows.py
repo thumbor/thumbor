4e9f82c93ecd6b3bd74ef50966c001a91ffe33d2
Sebastian Eichner <mail@sebastian-eichner.de>
2015-09-22 18:37:09 +0200

    implemented first attempt on keeping subsampling and qtables after image manipulations

--- vows/pil_engine_vows.py
+++ vows/pil_engine_vows.py
@@ -8,10 +8,17 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com timehome@corp.globo.com
 
+from thumbor.context import Context, RequestParameters, ServerParameters
+from thumbor.config import Config
+from thumbor.importer import Importer
+
 from pyvows import Vows, expect
 ctx = Vows.Context
 
 import thumbor.engines.pil as PIL
+from os.path import join, abspath, dirname
+
+FIXTURES_FOLDER = join(abspath(dirname(__file__)), 'fixtures')
 
 class MockImage:
     def __init__(self, mode, width, height):
@@ -19,6 +26,7 @@ class MockImage:
         self.width = width
         self.height = height
         self.calls = []
+
     def convert(self, mode):
         self.calls.append('convert')
         self.mode = mode
@@ -31,6 +39,52 @@ class MockImage:
 
 @Vows.batch
 class PilEngineVows(ctx):
+
+    class ReadingJpegImage(ctx):
+        def topic(self):
+            config = Config()
+            server = ServerParameters(
+                8889, 'localhost', 'thumbor.conf', None, 'info', None
+            )
+
+            context = Context(server, config, Importer(config))
+
+            with open("%s/quantization.jpg" % FIXTURES_FOLDER, "rb") as f:
+                buffer = f.read()
+
+            engine = PIL.Engine(context=context)
+            engine.load(buffer, '.jpg')
+
+            return engine
+        def should_store_subsampling_in_engine(self, engine):
+            # "Possible subsampling values are 0, 1 and 2 that correspond to 4:4:4, 4:2:2 and 4:1:1 (or 4:2:0?)." (PIL/JpegPresets.py)
+            expect(engine.subsampling).to_equal(0)
+        def should_store_quantization_in_engine(self, engine):
+            expect(len(engine.qtables)).to_equal(2)
+
+    class WritingJpegImage(ctx):
+        def topic(self):
+            config = Config()
+            server = ServerParameters(
+                8889, 'localhost', 'thumbor.conf', None, 'info', None
+            )
+
+            context = Context(server, config, Importer(config))
+
+            with open("%s/quantization.jpg" % FIXTURES_FOLDER, "rb") as f:
+                buffer = f.read()
+
+            engine = PIL.Engine(context=context)
+            engine.load(buffer, '.jpg')
+            engine.read('.jpg', None);
+
+            return engine.image
+        def should_save_with_stored_subsampling_if_quality_is_None(self, image):
+            expect(image.encoderinfo.get('subsampling')).to_equal(0)
+        def should_save_with_stored_qtables_if_quality_is_None(self, image):
+            expect(len(image.encoderinfo.get('qtables'))).to_equal(2)
+
+
 
     class ResizedPaletteImage(ctx):
         def topic(self):
