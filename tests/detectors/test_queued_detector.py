fdfdcbf146fbb34834c178bae4b071f58855c00b
Bernardo Heynemann <heynemann@gmail.com>
2015-10-02 20:24:01 -0300

    Queued Detector tests.

--- tests/detectors/test_queued_detector.py
+++ tests/detectors/test_queued_detector.py
@@ -30,6 +30,7 @@ class QueuedDetectorTestCase(TestCase):
 
         self.redis.delete('resque:unique:queue:Detect:/image/test.jpg')
         self.redis.delete('resque:queue:Detect')
+        QueuedDetector.queue = None
 
     def test_detector_sends_to_queue(self):
         ctx = mock.Mock(
@@ -66,3 +67,54 @@ class QueuedDetectorTestCase(TestCase):
 
         result = self.redis.lpop('resque:queue:Detect')
         expect(loads(result)).to_be_like(expected_payload)
+
+    def test_detector_fails_properly(self):
+        ctx = mock.Mock(
+            config=mock.Mock(
+                REDIS_QUEUE_SERVER_HOST='0.0.0.0',
+                REDIS_QUEUE_SERVER_PORT=6669,
+                REDIS_QUEUE_SERVER_DB=0,
+                REDIS_QUEUE_SERVER_PASSWORD='hey_you',
+            ),
+            request=mock.Mock(
+                image_url='/image/test.jpg',
+                detection_error=False,
+            ),
+        )
+
+        detector = QueuedDetector(ctx, 1, [])
+        expect(detector).not_to_be_null()
+
+        def validate(data):
+            expect(data).to_be_empty()
+
+        detector.detect(validate)
+        expect(ctx.request.detection_error).to_be_true()
+        expect(detector.queue).to_be_null()
+
+    def test_detector_can_detect_twice(self):
+        ctx = mock.Mock(
+            config=mock.Mock(
+                REDIS_QUEUE_SERVER_HOST='0.0.0.0',
+                REDIS_QUEUE_SERVER_PORT=6668,
+                REDIS_QUEUE_SERVER_DB=0,
+                REDIS_QUEUE_SERVER_PASSWORD='hey_you',
+            ),
+            request=mock.Mock(
+                image_url='/image/test.jpg',
+                detection_error=False,
+            ),
+        )
+
+        detector = QueuedDetector(ctx, 1, [])
+        expect(detector).not_to_be_null()
+
+        def validate(data):
+            expect(data).to_be_empty()
+
+        detector.detect(validate)
+        expect(ctx.request.detection_error).to_be_false()
+        expect(detector.queue).not_to_be_null()
+
+        detector.detect(validate)
+        expect(detector.queue).not_to_be_null()
