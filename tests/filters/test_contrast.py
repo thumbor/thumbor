001b39e635b22abff0b7f10253907ca97329dd67
Bernardo Heynemann <heynemann@gmail.com>
2015-10-08 11:26:54 -0300

    Contrast filter tested

---
+++ tests/filters/test_contrast.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com timehome@corp.globo.com
+
+from preggy import expect
+
+from tests.base import FilterTestCase
+
+
+class BlurFilterTestCase(FilterTestCase):
+    def test_contrast_filter(self):
+        image = self.get_filtered('source.jpg', 'thumbor.filters.contrast', 'contrast(20)')
+        expected = self.get_fixture('contrast.jpg')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.98)
