be8f590e8b146ebe4e04426a2fe357ddf1a38eaf
Bernardo Heynemann <heynemann@gmail.com>
2015-10-08 15:14:12 -0300

    Fill filter tested and covered.
    This was a really tough one, since it required fit-in to be enabled as
    well as particular dimensions to be used.
    
    Also images must have the same size (that includes number of channels),
    so I had to convert both fixture and filtered images to RGBA. Just
    leaving this here if someone wonders why the changes were made to the
    base test.

--- tests/base.py
+++ tests/base.py
@@ -17,9 +17,10 @@ from PIL import Image
 from skimage.measure import structural_similarity
 
 from thumbor.app import ThumborServiceApp
-from thumbor.context import Context
+from thumbor.context import Context, RequestParameters
 from thumbor.config import Config
 from thumbor.importer import Importer
+from thumbor.transformer import Transformer
 
 from tornado.testing import AsyncHTTPTestCase
 
@@ -33,15 +34,27 @@ class TestCase(AsyncHTTPTestCase):
 
 
 class FilterTestCase(PythonTestCase):
-    def get_filter(self, filter_name, params_string=""):
+    def get_filter(self, filter_name, params_string="", config_context=None):
         config = Config(
             FILTERS=[filter_name],
         )
         importer = Importer(config)
         importer.import_modules()
+
+        req = RequestParameters()
+
         context = Context(config=config, importer=importer)
+        context.request = req
+        context.request.engine = context.modules.engine
+
+        if config_context is not None:
+            config_context(context)
+
         fltr = importer.filters[0]
         fltr.pre_compile()
+
+        context.transformer = Transformer(context)
+
         return fltr(params_string, context=context)
 
     def get_fixture_path(self, name):
@@ -49,23 +62,43 @@ class FilterTestCase(PythonTestCase):
 
     def get_fixture(self, name):
         im = Image.open(self.get_fixture_path(name))
+        im = im.convert('RGBA')
         return np.array(im)
 
-    def get_filtered(self, source_image, filter_name, params_string):
-        fltr = self.get_filter(filter_name, params_string)
+    def get_filtered(self, source_image, filter_name, params_string, config_context=None):
+        fltr = self.get_filter(filter_name, params_string, config_context)
         im = Image.open(self.get_fixture_path(source_image))
         img_buffer = BytesIO()
         im.save(img_buffer, 'JPEG', quality=100)
+
         fltr.engine.load(img_buffer.getvalue(), '.jpg')
+        fltr.context.transformer.img_operation_worker()
 
         fltr.run()
+
+        fltr.engine.image = fltr.engine.image.convert('RGBA')
+
         return np.array(fltr.engine.image)
 
     def get_ssim(self, actual, expected):
-        return structural_similarity(actual, expected, multichannel=True)
+        im = Image.fromarray(actual)
+        im2 = Image.fromarray(expected)
+
+        if im.size[0] != im2.size[0] or im.size[1] != im2.size[1]:
+            raise RuntimeError(
+                "Can't calculate SSIM for images of different sizes (one is %dx%d, the other %dx%d)." % (
+                    im.size[0], im.size[1],
+                    im2.size[0], im2.size[1],
+                )
+            )
+        return structural_similarity(np.array(im), np.array(im2), multichannel=True)
 
     def debug(self, image):
         im = Image.fromarray(image)
         path = '/tmp/debug_image_%s.jpg' % random.randint(1, 10000)
         im.save(path, 'JPEG')
         print 'The debug image was in %s.' % path
+
+    def debug_size(self, image):
+        im = Image.fromarray(image)
+        print "Image dimensions are %dx%d (shape is %s)" % (im.size[0], im.size[1], image.shape)
---
+++ tests/filters/test_fill.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com timehome@corp.globo.com
+
+
+from preggy import expect
+
+from tests.base import FilterTestCase
+
+
+class FillFilterTestCase(FilterTestCase):
+    def test_fill_filter_with_fixed_color(self):
+        def config_context(context):
+            context.request.fit_in = True
+            context.request.width = 400
+            context.request.height = 400
+
+        image = self.get_filtered(
+            'source.jpg',
+            'thumbor.filters.fill',
+            'fill(0000ff)',
+            config_context=config_context
+        )
+
+        expected = self.get_fixture('fill.jpg')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.97)
+
+    def test_fill_filter_with_average(self):
+        def config_context(context):
+            context.request.fit_in = True
+            context.request.width = 400
+            context.request.height = 400
+
+        image = self.get_filtered(
+            'source.jpg',
+            'thumbor.filters.fill',
+            'fill(auto)',
+            config_context=config_context
+        )
+
+        expected = self.get_fixture('fill2.jpg')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.98)
