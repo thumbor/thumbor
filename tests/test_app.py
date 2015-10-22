2dee19a4df2bb7cbb637392bfa46ece810609a23
Bernardo Heynemann <heynemann@gmail.com>
2015-10-22 18:36:48 -0200

    Thumbor App test suite

---
+++ tests/test_app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com timehome@corp.globo.com
+
+from unittest import TestCase
+import mock
+
+from preggy import expect
+
+from thumbor.app import (
+    ThumborServiceApp
+)
+from thumbor.url import Url
+
+from nose_focus import focus  # NOQA
+
+
+@focus  # NOQA
+class AppTestCase(TestCase):
+    def test_can_create_app(self):
+        ctx = mock.Mock()
+        app = ThumborServiceApp(ctx)
+        expect(app).not_to_be_null()
+        expect(app.context).to_equal(ctx)
+
+    def test_can_get_handlers(self):
+        ctx = mock.Mock(
+            config=mock.Mock(
+                UPLOAD_ENABLED=False,
+                USE_BLACKLIST=False,
+            )
+        )
+        app = ThumborServiceApp(ctx)
+
+        handlers = app.get_handlers()
+        expect(handlers).to_length(2)
+        expect(handlers[0][0]).to_equal(r'/healthcheck')
+        expect(handlers[1][0]).to_equal(Url.regex())
+
+    def test_can_get_handlers_with_upload(self):
+        ctx = mock.Mock(
+            config=mock.Mock(
+                UPLOAD_ENABLED=True,
+                USE_BLACKLIST=False,
+            )
+        )
+        app = ThumborServiceApp(ctx)
+
+        handlers = app.get_handlers()
+        expect(handlers).to_length(5)
+
+    def test_can_get_handlers_with_blacklist(self):
+        ctx = mock.Mock(
+            config=mock.Mock(
+                UPLOAD_ENABLED=False,
+                USE_BLACKLIST=True,
+            )
+        )
+        app = ThumborServiceApp(ctx)
+
+        handlers = app.get_handlers()
+        expect(handlers).to_length(3)
