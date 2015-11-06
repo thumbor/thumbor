8e769c5d291e2566ecd191f2fc683ec71382bf22
adeboisanger <adeboisanger@figarocms.fr>
2015-11-06 09:34:33 +0000

    Added Unit Tests, refs issue #545
    Removed execution floag on test_watermark.py, refs issue #545
    
    Regenerated tests images with Quality = 100, refs issue #545
    
    Regenerated Repeat tests images with Quality = 100, refs issue #545
    
    Changed similarity threshold, refs issue #545
    
    Changed test source picture, refs issue #545
    
    Fixing tests
    
    Add Debug
    
    Change sim expectation

---
+++ tests/filters/test_watermark.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com timehome@corp.globo.com
+
+from preggy import expect
+
+from tests.base import FilterTestCase
+
+
+class WatermarkFilterTestCase(FilterTestCase):
+    def test_watermark_filter_centered(self):
+        image = self.get_filtered('source100.jpg', 'thumbor.filters.watermark', 'watermark(watermark.png,center,center,60)')
+        expected = self.get_fixture('watermarkCenter.jpg')
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_watermark_filter_centered_x(self):
+        image = self.get_filtered('source100.jpg', 'thumbor.filters.watermark', 'watermark(watermark.png,center,40,20)')
+        expected = self.get_fixture('watermarkCenterX.jpg')
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_watermark_filter_centered_y(self):
+        image = self.get_filtered('source100.jpg', 'thumbor.filters.watermark', 'watermark(watermark.png,80,center,50)')
+        expected = self.get_fixture('watermarkCenterY.jpg')
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_watermark_filter_repeated(self):
+        image = self.get_filtered('source100.jpg', 'thumbor.filters.watermark', 'watermark(watermark.png,repeat,repeat,70)')
+        expected = self.get_fixture('watermarkRepeat.jpg')
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.92)
+
+    def test_watermark_filter_repeated_x(self):
+        image = self.get_filtered('source100.jpg', 'thumbor.filters.watermark', 'watermark(watermark.png,repeat,center,70)')
+        expected = self.get_fixture('watermarkRepeatX.jpg')
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.94)
+
+    def test_watermark_filter_repeated_y(self):
+        image = self.get_filtered('source100.jpg', 'thumbor.filters.watermark', 'watermark(watermark.png,30,repeat,30)')
+        expected = self.get_fixture('watermarkRepeatY.jpg')
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.94)
+
+    def test_watermark_filter_simple(self):
+        image = self.get_filtered('source100.jpg', 'thumbor.filters.watermark', 'watermark(watermark.png,30,-50,60)')
+        expected = self.get_fixture('watermarkSimple.jpg')
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
