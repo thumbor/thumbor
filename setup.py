c5ad4e94cf8a94d9632add8c9c3ef1c7bb5f1c35
Gilles Dubuc <gdubuc@wikimedia.org>
2016-06-02 14:18:02 +0200

    Relax pycurl version requirement

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ http://<thumbor-server>/300x200/smart/s.glbimg.com/et/bb/f/original/2011/03/24/V
         install_requires=[
             "tornado>=4.1.0,<5.0.0",
             "pyCrypto>=2.1.0",
-            "pycurl>=7.19.0,<7.20.0",
+            "pycurl>=7.19.0,<7.44.0",
             "Pillow>=3.0.0,<4.0.0",
             "derpconf>=0.2.0",
             "pexif>=0.15,<1.0",
