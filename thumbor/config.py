688c22a7accd51cf16e75c259f46203e3e1c2dff
Denis Khabarov <admin@saymon21-root.pro>
2016-10-27 15:10:04 +0300

    added config for certificates validation

--- thumbor/config.py
+++ thumbor/config.py
@@ -176,6 +176,9 @@ Config.define(
     'HTTP_LOADER_CA_CERTS', None,
     'The filename of CA certificates in PEM format', 'HTTP Loader')
 Config.define(
+    'HTTP_LOADER_VALIDATE_CERTS', True,
+    'Validate the server’s certificate for HTTPS requests', 'HTTP Loader')
+Config.define(
     'HTTP_LOADER_CLIENT_KEY', None,
     'The filename for client SSL key', 'HTTP Loader')
 Config.define(
