3cd34eb8f7562c746f0fcbb8fa735e9360451ce5
Konstantin Kopachev <tenzzor@gmail.com>
2017-04-03 11:42:32 -0700

    Jpegtran should preserve ICC profile if not forced to strip + more tests

--- tests/optimizers/test_jpegtran.py
+++ tests/optimizers/test_jpegtran.py
@@ -8,22 +8,31 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
-from os.path import abspath, join, dirname
-
 from thumbor.config import Config
-from thumbor.context import Context
+from thumbor.context import Context, RequestParameters
 from thumbor.optimizers.jpegtran import Optimizer
-from thumbor.utils import which
 
 from unittest import TestCase
 
+import mock
+
 
 class JpegtranOptimizerTest(TestCase):
-    def get_optimizer(self):
+    def setUp(self):
+        self.patcher = mock.patch('thumbor.optimizers.jpegtran.Popen')
+        self.mock_popen = self.patcher.start()
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def get_optimizer(self, filters=None, progressive=False):
         conf = Config()
         conf.STATSD_HOST = ''
-        conf.JPEGTRAN_PATH = which('jpegtran')
+        conf.JPEGTRAN_PATH = '/somewhere/jpegtran'
+        conf.PROGRESSIVE_JPEG = progressive
+        req = RequestParameters(filters=filters)
         ctx = Context(config=conf)
+        ctx.request = req
         optimizer = Optimizer(ctx)
 
         return optimizer
@@ -41,18 +50,21 @@ class JpegtranOptimizerTest(TestCase):
         self.assertFalse(optimizer.should_run('.gif', ''))
 
     def test_should_optimize(self):
-        optimizer = self.get_optimizer()
-        fixtures = abspath(join(dirname(__file__), '../fixtures/images/'))
-        with open(fixtures + '/Christophe_Henner_-_June_2016.jpg', 'rb') as f:
-            input_bufer = f.read()
-        return_buffer = optimizer.run_optimizer('.jpg', input_bufer)
+        input = 'input buffer'
+        output = 'output buffer'
+        self.mock_popen.return_value.returncode = 0
+        self.mock_popen.return_value.communicate.return_value = (output, "Error")
 
-        self.assertLessEqual(len(return_buffer), len(input_bufer),
-                             "jpegtran could not lower filesize for images/image.jpg")
+        optimizer = self.get_optimizer()
+        return_buffer = optimizer.run_optimizer('.jpg', input)
+
+        self.mock_popen.return_value.communicate.assert_called_with(input)
+        self.assertEqual(output, return_buffer)
 
     def test_should_return_old_buffer_for_invalid_extension(self):
         optimizer = self.get_optimizer()
         buffer = 'garbage'
+
         return_buffer = optimizer.run_optimizer('.png', buffer)
 
         self.assertEqual(return_buffer, buffer)
@@ -60,6 +72,63 @@ class JpegtranOptimizerTest(TestCase):
     def test_should_return_old_buffer_for_invalid_image(self):
         optimizer = self.get_optimizer()
         buffer = 'garbage'
+
+        self.mock_popen.return_value.returncode = 1
+        self.mock_popen.return_value.communicate.return_value = ('Output', 'Error')
+
         return_buffer = optimizer.run_optimizer('.jpg', buffer)
 
         self.assertEqual(return_buffer, buffer)
+
+    def test_should_preserve_comments_if_strip_icc_filter_set(self):
+        self.mock_popen.return_value.returncode = 0
+        self.mock_popen.return_value.communicate.return_value = ('Output', 'Error')
+
+        optimizer = self.get_optimizer(filters=['strip_icc'])
+        optimizer.run_optimizer('.jpg', '')
+
+        command = self.mock_popen.call_args[0][0]
+
+        self.assertIn('-copy', command)
+        self.assertIn('comments', command)
+        self.assertNotIn('all', command)
+
+        optimizer = self.get_optimizer()
+        optimizer.run_optimizer('.jpg', '')
+
+        command = self.mock_popen.call_args[0][0]
+
+        self.assertIn('-copy', command)
+        self.assertIn('all', command)
+        self.assertNotIn('comments', command)
+
+    def test_should_make_progressive_when_configured(self):
+        self.mock_popen.return_value.returncode = 0
+        self.mock_popen.return_value.communicate.return_value = ('Output', 'Error')
+
+        optimizer = self.get_optimizer(progressive=False)
+        optimizer.run_optimizer('.jpg', '')
+
+        args, _ = self.mock_popen.call_args
+        command = args[0]
+
+        self.assertNotIn('-progressive', command)
+
+        optimizer = self.get_optimizer(progressive=True)
+        optimizer.run_optimizer('.jpg', '')
+
+        args, _ = self.mock_popen.call_args
+        command = args[0]
+
+        self.assertIn('-progressive', command)
+
+    @mock.patch('thumbor.optimizers.jpegtran.logger.warn')
+    def test_should_log_warning_when_failed(self, warn_logger):
+        optimizer = self.get_optimizer()
+
+        self.mock_popen.return_value.returncode = 1
+        self.mock_popen.return_value.communicate.return_value = ('Output', 'Error')
+
+        optimizer.run_optimizer('.jpg', 'garbage')
+
+        warn_logger.assert_called_once()
