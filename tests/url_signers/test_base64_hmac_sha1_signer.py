6b18e89b818d40f8572b11af6b294cc20cf4a8f2
cclauss <cclauss@bluewin.ch>
2017-07-23 21:18:14 +0200

    Update test_base64_hmac_sha1_signer.py

--- tests/url_signers/test_base64_hmac_sha1_signer.py
+++ tests/url_signers/test_base64_hmac_sha1_signer.py
@@ -15,14 +15,14 @@ from unittest import TestCase
 
 from preggy import expect
 
+from thumbor.url_signers.base64_hmac_sha1 import (
+    UrlSigner
+)
+
 try:
     unicode        # Python 2
 except NameError:
     unicode = str  # Python 3
-
-from thumbor.url_signers.base64_hmac_sha1 import (
-    UrlSigner
-)
 
 
 class Base64HmacSha1UrlSignerTestCase(TestCase):
