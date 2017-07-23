59a8ba3addd1e448f491e28bb0ed4d86c6ac624a
cclauss <cclauss@bluewin.ch>
2017-07-23 16:50:26 +0200

    basestring for Python 3

--- thumbor/error_handlers/file.py
+++ thumbor/error_handlers/file.py
@@ -9,6 +9,11 @@ import time
 
 from thumbor import __version__
 
+try:
+    basestring        # Python 2
+except NameError:
+    basestring = str  # Python 3
+
 
 class ErrorHandler(object):
     def __init__(self, config):
