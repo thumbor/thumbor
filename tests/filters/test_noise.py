1d5ecf1a9825f3d8b094c3cf0965204f76a74172
Konstantin Kopachev <tenzzor@gmail.com>
2017-09-06 11:29:54 -0700

    Update noise filter test image to be png, so SSIM will work

--- tests/filters/test_noise.py
+++ tests/filters/test_noise.py
@@ -9,18 +9,15 @@
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
 
-import unittest
-
 from preggy import expect
 
 from tests.base import FilterTestCase
 
 
-@unittest.skip("Does not work in CI - Comparison of image failing in Ubuntu")
 class NoiseFilterTestCase(FilterTestCase):
     def test_noise_filter(self):
         image = self.get_filtered('source.jpg', 'thumbor.filters.noise', 'noise(200,123)')
-        expected = self.get_fixture('noise.jpg')
+        expected = self.get_fixture('noise.png')
 
         ssim = self.get_ssim(image, expected)
         expect(ssim).to_be_greater_than(0.98)
