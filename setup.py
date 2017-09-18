d56bea004418bf4e21a4a6347f8845bb58d8e958
Marcelo Jorge Vieira <metal@alucinados.com>
2017-09-18 19:59:41 -0300

    Replaced 'pexif' dependency with 'piexif'
    Replacing because piexif supports Python 2.7, 3.3, 3.4, 3.5, pypy, and pypy3;
    pexif only supports Python 2.7.

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ http://<thumbor-server>/300x200/smart/s.glbimg.com/et/bb/f/original/2011/03/24/V
             "pycurl>=7.19.0,<7.44.0",
             "Pillow>=3.0.0,<5.0.0",
             "derpconf>=0.2.0",
-            "pexif>=0.15,<1.0",
+            "piexif>=1.0.13,<1.1.0",
             "statsd>=3.0.1",
             "libthumbor>=1.3.2",
             "futures",
