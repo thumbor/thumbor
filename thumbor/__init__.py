a051bdb43fd8986eaa2601d5765cc9ee62d13807
Carlos Gusmão <carlos.gusmao@corp.globo.com>
2018-06-15 14:00:40 -0300

    Release 6.5.1

--- thumbor/__init__.py
+++ thumbor/__init__.py
@@ -9,5 +9,5 @@
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 '''This is the main module in thumbor'''
 
-__version__ = "6.5.0"
-__release_date__ = "11-Jun-2018"
+__version__ = "6.5.1"
+__release_date__ = "15-Jun-2018"
