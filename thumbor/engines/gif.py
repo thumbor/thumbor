7570f5aa6d92aa9eb15ff6ecbdfa44b852c895cf
Daniel Brooks <bass_rock@me.com>
2018-08-25 22:58:59 -0700

    Adding the absolute import for pil. #1074

--- thumbor/engines/gif.py
+++ thumbor/engines/gif.py
@@ -8,6 +8,8 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
+from __future__ import absolute_import
+
 from io import BytesIO
 from PIL import Image
 import re
