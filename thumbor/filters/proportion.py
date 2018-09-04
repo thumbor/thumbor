5c13f36cf772ddc9316cba1f06775134e7a0bb3e
Udlei Nati <udlei.nati@free2move.me>
2018-09-04 10:40:57 +0100

    Add proportion filter

---
+++ thumbor/filters/proportion.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com thumbor@googlegroups.com
+
+from thumbor.filters import BaseFilter, filter_method
+
+
+class Filter(BaseFilter):
+
+    @filter_method(BaseFilter.DecimalNumber)
+    def proportion(self, value):
+        source_width, source_height = self.context.request.engine.size
+
+        new_width = source_width * value
+        new_height = source_height * value
+
+        if new_width <= 0 or new_height <= 0:
+            return
+
+        self.engine.resize(new_width, new_height)
