cf0978a84711dfe8925fb19f0d559fa5979a4698
Bernardo Heynemann <heynemann@gmail.com>
2018-11-11 23:24:27 -0200

    File Loader and docs

--- thumbor/blueprints/loaders/file.py
+++ thumbor/blueprints/loaders/file.py
@@ -13,29 +13,35 @@ from os.path import abspath, exists
 
 import tornado.gen
 
-from thumbor.lifecycle import Events
+from thumbor.blueprints.loaders import Loader as BaseLoader
 
 
 def plug_into_lifecycle():
-    Events.subscribe(Events.Imaging.load_source_image, on_load_source_image)
+    FileLoader()
 
 
-@tornado.gen.coroutine
-def on_load_source_image(sender, request, details):
-    request_parameters = details.request_parameters
+class FileLoader(BaseLoader):
+    @classmethod
+    @tornado.gen.coroutine
+    def load_source_image(cls, request, details):
+        request_parameters = details.request_parameters
 
-    file_path = "%s/%s" % (details.config.FILE_LOADER_ROOT_PATH.rstrip('/'),
-                           request_parameters.image_url.lstrip('/'))
-    file_path = abspath(file_path)
-    inside_root_path = file_path.startswith(
-        abspath(details.config.FILE_LOADER_ROOT_PATH))
+        file_path = "%s/%s" % (
+            details.config.FILE_LOADER_ROOT_PATH.rstrip("/"),
+            request_parameters.image_url.lstrip("/"),
+        )
+        file_path = abspath(file_path)
+        inside_root_path = file_path.startswith(
+            abspath(details.config.FILE_LOADER_ROOT_PATH)
+        )
 
-    if inside_root_path and exists(file_path):
-        with open(file_path, 'rb') as image:
-            details.source_image = image.read()
-    else:
+        if inside_root_path and exists(file_path):
+            with open(file_path, "rb") as image:
+                details.source_image = image.read()
+
+            return
+
         details.status_code = 502
         details.finish_early = True
-        details.headers['Thumbor-File-Loader-Result'] = 'Image Not Found'
+        details.headers["Thumbor-File-Loader-Result"] = "Image Not Found"
         details.body = "Could not load source image"
-        return
