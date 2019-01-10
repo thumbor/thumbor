ad8d3813bcd6875b890e43c1c39955d6a47b2c22
Bernardo Heynemann <heynemann@gmail.com>
2019-01-10 11:09:12 -0200

    release 6.6.1

--- thumbor/__init__.py
+++ thumbor/__init__.py
@@ -9,5 +9,5 @@
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 '''This is the main module in thumbor'''
 
-__version__ = "6.6.0"
-__release_date__ = "07-Nov-2018"
+__version__ = "6.6.1"
+__release_date__ = "10-Jan-2019"
