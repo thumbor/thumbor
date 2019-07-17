f1d132187a3ef4ce8f790e8adc70ddd7826e29a3
Konstantin Kopachev <tenzzor@gmail.com>
2019-07-16 23:01:38 -0700

    Replace piexif with custom orientation-only exif reader/writer

--- setup.py
+++ setup.py
@@ -100,7 +100,6 @@ http://<thumbor-server>/300x200/smart/s.glbimg.com/et/bb/f/original/2011/03/24/V
             "pycurl>=7.19.0,<7.44.0",
             "Pillow>=4.3.0,<6.0.0",
             "derpconf>=0.2.0",
-            "piexif>=1.0.13,<2.0.0",
             "statsd>=3.0.1",
             "libthumbor>=1.3.2",
             "futures",
