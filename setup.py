5f753309fab0482d12208bbe378692f1f7867432
Bernardo Heynemann <heynemann@gmail.com>
2020-02-01 15:50:14 +0000

    Updating test running and Linux support in tests
    This commit updates the way Coverage.py expects to be run.
    
    Black was also added as a dependency and should be used by
    all committers to format thumbor codebase.
    
    Compatibilized a test in test_utils to work on linux as well
    as mac os.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ tests_require = [
     "colorama",
     "numpy",
     "flake8",
+    "black",
     "yanc",
     "remotecv",
     "pyssim>=0.4.0",
@@ -39,67 +40,80 @@ tests_require = [
 
 def filter_extension_module(name, lib_objs, lib_headers):
     return Extension(
-        'thumbor.ext.filters.%s' % name,
-        ['thumbor/ext/filters/%s.c' % name] + lib_objs,
-        libraries=['m'],
-        include_dirs=['thumbor/ext/filters/lib'],
-        depends=['setup.py'] + lib_objs + lib_headers,
-        extra_compile_args=['-Wall', '-Wextra', '-Werror', '-Wno-unused-parameter'])
+        "thumbor.ext.filters.%s" % name,
+        ["thumbor/ext/filters/%s.c" % name] + lib_objs,
+        libraries=["m"],
+        include_dirs=["thumbor/ext/filters/lib"],
+        depends=["setup.py"] + lib_objs + lib_headers,
+        extra_compile_args=[
+            "-Wall",
+            "-Wextra",
+            "-Werror",
+            "-Wno-unused-parameter",
+        ],
+    )
 
 
 def gather_filter_extensions():
-    files = glob.glob('thumbor/ext/filters/_*.c')
-    lib_objs = glob.glob('thumbor/ext/filters/lib/*.c')
-    lib_headers = glob.glob('thumbor/ext/filters/lib/*.h')
+    files = glob.glob("thumbor/ext/filters/_*.c")
+    lib_objs = glob.glob("thumbor/ext/filters/lib/*.c")
+    lib_headers = glob.glob("thumbor/ext/filters/lib/*.h")
 
-    return [filter_extension_module(f[0:-2].split('/')[-1], lib_objs, lib_headers) for f in files]
+    return [
+        filter_extension_module(f[0:-2].split("/")[-1], lib_objs, lib_headers)
+        for f in files
+    ]
 
 
 def run_setup(extension_modules=[]):
-    if 'CFLAGS' not in os.environ:
-        os.environ['CFLAGS'] = ''
+    if "CFLAGS" not in os.environ:
+        os.environ["CFLAGS"] = ""
     setup(
-        name='thumbor',
+        name="thumbor",
         version=__version__,
-        description="thumbor is an open-source photo thumbnail service by globo.com",
+        description=(
+            "thumbor is an open-source photo thumbnail service by globo.com"
+        ),
         long_description="""
-Thumbor is a smart imaging service. It enables on-demand crop, resizing and flipping of images.
+Thumbor is a smart imaging service. It enables on-demand crop,
+resizing and flipping of images.
 
-It also features a VERY smart detection of important points in the image for better cropping and
-resizing, using state-of-the-art face and feature detection algorithms (more on that in Detection Algorithms).
+It also features a VERY smart detection of important points in the
+image for better cropping and resizing, using state-of-the-art face
+and feature detection algorithms (more on that in Detection Algorithms).
 
-Using thumbor is very easy (after it is running). All you have to do is access it using an url for an image, like this:
+Using thumbor is very easy (after it is running). All you have to do
+is access it using an url for an image, like this:
 
 http://<thumbor-server>/300x200/smart/thumbor.readthedocs.io/en/latest/_images/logo-thumbor.png
 """,
-        keywords='imaging face detection feature thumbnail imagemagick pil opencv',
-        author='globo.com',
-        author_email='thumbor@googlegroups.com',
-        url='https://github.com/thumbor/thumbor/wiki',
-        license='MIT',
+        keywords=(
+            "imaging face detection feature thumbnail imagemagick pil opencv"
+        ),
+        author="globo.com",
+        author_email="thumbor@googlegroups.com",
+        url="https://github.com/thumbor/thumbor/wiki",
+        license="MIT",
         classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Operating System :: MacOS',
-            'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3 :: Only',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-            'Topic :: Multimedia :: Graphics :: Presentation'
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: MacOS",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+            "Topic :: Multimedia :: Graphics :: Presentation",
         ],
-        packages=['thumbor'],
+        packages=["thumbor"],
         package_dir={"thumbor": "thumbor"},
         include_package_data=True,
-        package_data={
-            '': ['*.xml'],
-        },
-
+        package_data={"": ["*.xml"]},
         install_requires=[
             "tornado>=4.1.0,<6.0.0",
             "pycurl>=7.19.0,<7.44.0",
@@ -110,28 +124,26 @@ http://<thumbor-server>/300x200/smart/thumbor.readthedocs.io/en/latest/_images/l
             "pytz",
             "webcolors",
         ],
-
-        extras_require={
-            'tests': tests_require,
-        },
-
+        extras_require={"tests": tests_require},
         entry_points={
-            'console_scripts': [
-                'thumbor=thumbor.server:main',
-                'thumbor-url=thumbor.url_composer:main',
-                'thumbor-config=thumbor.config:generate_config'
+            "console_scripts": [
+                "thumbor=thumbor.server:main",
+                "thumbor-url=thumbor.url_composer:main",
+                "thumbor-config=thumbor.config:generate_config",
             ],
         },
-
-        ext_modules=extension_modules
+        ext_modules=extension_modules,
     )
 
 
 try:
     run_setup(gather_filter_extensions())
 except SystemExit as exit:
-    print("\n\n*******************************************************************")
+    print("\n\n%s" % ("*" * 66))
     logging.exception(exit)
-    print("\n\n*******************************************************************")
-    print("Couldn't build one or more native extensions, skipping compilation.\n\n")
+    print("\n\n%s" % ("*" * 66))
+    print(
+        "Couldn't build one or more native extensions, "
+        "skipping compilation.\n\n"
+    )
     run_setup()
