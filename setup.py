2ff005a0f0b7e6ff48222f9d5bcc14b72371182f
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2020-03-19 19:22:32 +0530

    setup: Make thumbor installable on python 3.6 systems.
    Let pypi know that thumbor supports py3.6 using python_requires
    and classifiers.

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ http://<thumbor-server>/300x200/smart/thumbor.readthedocs.io/en/latest/_images/l
         author_email="thumbor@googlegroups.com",
         url="https://github.com/thumbor/thumbor/wiki",
         license="MIT",
-        python_requires=">=3.7",
+        python_requires=">=3.6",
         classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
@@ -103,6 +103,7 @@ http://<thumbor-server>/300x200/smart/thumbor.readthedocs.io/en/latest/_images/l
             "Operating System :: MacOS",
             "Operating System :: POSIX :: Linux",
             "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3 :: Only",
