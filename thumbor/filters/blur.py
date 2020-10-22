520c9f8d7905c88424f05f33b0257ee9ef8d3cda
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-10-22 13:06:59 +0200

    [FIX] Make fill with blur engine-agnostic
    The `fill` filter, when used with color set to "blur", depended on the
    engine having the `blur` method. That broke the filter when the engine
    in use doesn't fulfill such dependency.
    
    By using the already existing blur capabilities of the `blur` filter,
    `fill(blur)` now wokrs for engines other than PIL.

--- thumbor/filters/blur.py
+++ thumbor/filters/blur.py
@@ -15,6 +15,27 @@ from thumbor.filters import BaseFilter, filter_method
 MAX_RADIUS = 150
 
 
+def generate_1d_matrix(sigma, radius):
+    matrix_size = (radius * 2) + 1
+    matrix = []
+    two_sigma_squared = float(2 * sigma * sigma)
+    for column in range(matrix_size):
+        adj_x = column - radius
+        exp = math.e ** -(((adj_x * adj_x)) / two_sigma_squared)
+        matrix.append(exp / math.sqrt(two_sigma_squared * math.pi))
+    return tuple(matrix), matrix_size
+
+
+def apply_blur(mode, data, size, radius, sigma=0):
+    if sigma == 0:
+        sigma = radius
+    if radius > MAX_RADIUS:
+        radius = MAX_RADIUS
+    matrix, matrix_size = generate_1d_matrix(sigma, radius)
+    data = _convolution.apply(mode, data, size[0], size[1], matrix, matrix_size, True)
+    return _convolution.apply(mode, data, size[0], size[1], matrix, 1, True)
+
+
 class Filter(BaseFilter):
     """
         Usage: /filters:blur(<radius> [, <sigma>])
@@ -24,34 +45,8 @@ class Filter(BaseFilter):
             /filters:blur(4, 2)/
     """
 
-    def generate_1d_matrix(self, sigma, radius):
-        matrix_size = (radius * 2) + 1
-        matrix = []
-        two_sigma_squared = float(2 * sigma * sigma)
-        for column in range(matrix_size):
-            adj_x = column - radius
-            exp = math.e ** -(((adj_x * adj_x)) / two_sigma_squared)
-            matrix.append(exp / math.sqrt(two_sigma_squared * math.pi))
-        return tuple(matrix), matrix_size
-
     @filter_method(BaseFilter.PositiveNonZeroNumber, BaseFilter.DecimalNumber)
     async def blur(self, radius, sigma=0):
-        if sigma == 0:
-            sigma = radius
-        if radius > MAX_RADIUS:
-            radius = MAX_RADIUS
-        matrix, matrix_size = self.generate_1d_matrix(sigma, radius)
-        mode, data = self.engine.image_data_as_rgb()
-        imgdata = _convolution.apply(
-            mode,
-            data,
-            self.engine.size[0],
-            self.engine.size[1],
-            matrix,
-            matrix_size,
-            True,
-        )
-        imgdata = _convolution.apply(
-            mode, imgdata, self.engine.size[0], self.engine.size[1], matrix, 1, True,
-        )
+        mode, imgdata = self.engine.image_data_as_rgb()
+        imgdata = apply_blur(mode, imgdata, self.engine.size, radius, sigma)
         self.engine.set_image_data(imgdata)
