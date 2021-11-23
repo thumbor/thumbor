bfa174dd6690554befa6086fc3a9a492553ef1aa
Martin Güthler <mg@webfactory.de>
2021-11-23 10:45:57 +0100

    Allow sending a Access-Control-Allow-Origin header
    This commit takes #845 and adds tests and config support
    
    Co-authored-by: Christian Green <christian@fanhero.com>

--- tests/handlers/test_base_handler_with_http_headers.py
+++ tests/handlers/test_base_handler_with_http_headers.py
@@ -51,6 +51,53 @@ class ImageOperationsWithoutEtagsTestCase(BaseImagingTestCase):
         expect(response.headers).not_to_include("Etag")
 
 
+class ImageOperationsWithoutCorsHeaderTestCase(BaseImagingTestCase):
+    def get_context(self):
+        cfg = Config(SECURITY_KEY="ACME-SEC")
+        cfg.LOADER = "thumbor.loaders.file_loader"
+        cfg.FILE_LOADER_ROOT_PATH = self.loader_path
+        cfg.ENABLE_ETAGS = False
+
+        importer = Importer(cfg)
+        importer.import_modules()
+        server = ServerParameters(8889, "localhost", "thumbor.conf", None, "info", None)
+        server.security_key = "ACME-SEC"
+        return Context(server, cfg, importer)
+
+    @gen_test
+    async def test_can_get_image_cors_header(self):
+        response = await self.async_fetch(
+            "/unsafe/image.jpg", headers={"Accept": "image/webp,*/*;q=0.8"}
+        )
+
+        expect(response.code).to_equal(200)
+        expect(response.headers).not_to_include("Access-Control-Allow-Origin")
+
+
+class ImageOperationsWithCorsHeaderTestCase(BaseImagingTestCase):
+    def get_context(self):
+        cfg = Config(SECURITY_KEY="ACME-SEC", ACCESS_CONTROL_ALLOW_ORIGIN_HEADER='*')
+        cfg.LOADER = "thumbor.loaders.file_loader"
+        cfg.FILE_LOADER_ROOT_PATH = self.loader_path
+        cfg.ENABLE_ETAGS = False
+
+        importer = Importer(cfg)
+        importer.import_modules()
+        server = ServerParameters(8889, "localhost", "thumbor.conf", None, "info", None)
+        server.security_key = "ACME-SEC"
+        return Context(server, cfg, importer)
+
+    @gen_test
+    async def test_can_get_image_with_cors_header(self):
+        response = await self.async_fetch(
+            "/unsafe/image.jpg", headers={"Accept": "image/webp,*/*;q=0.8"}
+        )
+
+        expect(response.code).to_equal(200)
+        expect(response.headers).to_include("Access-Control-Allow-Origin")
+        expect(response.headers['Access-Control-Allow-Origin']).to_equal('*')
+
+
 class ImageOperationsWithLastModifiedTestCase(BaseImagingTestCase):
     def get_context(self):
         cfg = Config(SECURITY_KEY="ACME-SEC")
