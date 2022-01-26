85c58ef970a53ba6c1aa02c9438a4fc97633968d
Bernardo Heynemann <heynemann@gmail.com>
2022-01-26 14:13:02 -0300

    Allowed sources work in file_loader_http_fallback
    This commit fixes a bug with the
    file_loader_http_fallback loader in that it
    would not validate the requests being made

--- thumbor/loaders/file_loader_http_fallback.py
+++ thumbor/loaders/file_loader_http_fallback.py
@@ -8,14 +8,25 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
-from thumbor.loaders import file_loader, http_loader
+from thumbor.loaders import LoaderResult, file_loader, http_loader
 
 
 async def load(context, path):
     # First attempt to load with file_loader
     result = await file_loader.load(context, path)
+
     if result.successful:
         return result
 
     # If file_loader failed try http_loader
+
+    if not http_loader.validate(context, path):
+        result = LoaderResult()
+        result.successful = False
+        result.error = LoaderResult.ERROR_BAD_REQUEST
+        result.extras["reason"] = "Unallowed domain"
+        result.extras["source"] = path
+
+        return result
+
     return await http_loader.load(context, path)
