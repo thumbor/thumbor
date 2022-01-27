aa8c49967aa02987080a97de40869d176b29b956
Bernardo Heynemann <heynemann@gmail.com>
2022-01-27 15:11:21 -0300

    Removed opencv dependency
    Now OpenCV is optional and detectors are smart to
    skip if cv2 could not be imported.
    Also refactored face detector a bit to make it more
    maintainable.
    Now thumbor can be installed with
    pip install thumbor
    pip install thumbor[all]
    pip install thumbor[opencv]
    pip install thumbor[tests]

--- thumbor/detectors/feature_detector/__init__.py
+++ thumbor/detectors/feature_detector/__init__.py
@@ -8,9 +8,9 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
-import cv2
 import numpy as np
 
+import cv2
 from thumbor.detectors import BaseDetector
 from thumbor.point import FocalPoint
 from thumbor.utils import logger
@@ -18,12 +18,22 @@ from thumbor.utils import logger
 
 class Detector(BaseDetector):
     async def detect(self):
+        if not self.verify_cv():
+            await self.next()
+
+            return
+
         engine = self.context.modules.engine
         try:
-            img = np.array(engine.convert_to_grayscale(update_image=False, alpha=False))
+            img = np.array(
+                engine.convert_to_grayscale(update_image=False, alpha=False)
+            )
         except Exception as error:
             logger.exception(error)
-            logger.warning("Error during feature detection; skipping to next detector")
+            logger.warning(
+                "Error during feature detection; skipping to next detector"
+            )
+
             return await self.next()  # pylint: disable=not-callable
 
         points = cv2.goodFeaturesToTrack(  # pylint: disable=no-member
@@ -33,12 +43,14 @@ class Detector(BaseDetector):
             minDistance=1.0,
             useHarrisDetector=False,
         )
+
         if points is not None:
             for point in points:
                 x_pos, y_pos = point.ravel()
                 self.context.request.focal_points.append(
                     FocalPoint(x_pos.item(), y_pos.item(), 1)
                 )
+
             return
 
         await self.next()  # pylint: disable=not-callable
