46232638a310affa0b81477bedb2390854f14c56
Guilherme Souza <101073+guilhermef@users.noreply.github.com>
2022-03-15 00:42:44 +0100

    Fix lint issue

--- tests/optimizers/test_gifv.py
+++ tests/optimizers/test_gifv.py
@@ -13,8 +13,9 @@ from unittest import TestCase, mock
 
 from thumbor.config import Config
 from thumbor.context import Context, RequestParameters
-from thumbor_plugins.optimizers.gifv import Optimizer
 from thumbor.utils import EXTENSION
+
+from thumbor_plugins.optimizers.gifv import Optimizer
 
 
 class GifvOptimizerTest(TestCase):
