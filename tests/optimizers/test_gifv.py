5c76684df566a0bd1e279b9d191d3a8f77fc011b
Guilherme Souza <101073+guilhermef@users.noreply.github.com>
2022-03-15 23:37:21 +0100

    Fix gifv unit tests

--- tests/optimizers/test_gifv.py
+++ tests/optimizers/test_gifv.py
@@ -11,68 +11,15 @@
 from shutil import which
 from unittest import TestCase, mock
 
-from thumbor.config import Config
-from thumbor.context import Context, RequestParameters
-from thumbor.utils import EXTENSION
+import thumbor_plugins.optimizers.gifv.Optimizer
 
+import thumbor.optimizers.gifv.Optimizer
 
 class GifvOptimizerTest(TestCase):
-    def setUp(self):
-        self.os_path_exists_patcher = mock.patch(
-            "thumbor.optimizers.gifv.exists"
-        )
-        self.mock_os_path_exists = self.os_path_exists_patcher.start()
-
-    def tearDown(self):
-        self.os_path_exists_patcher.stop()
-
-    def get_context(self):
-        conf = Config()
-        conf.STATSD_HOST = ""
-        conf.FFMPEG_PATH = which("ffmpeg")
-        ctx = Context(config=conf)
-        ctx.request = RequestParameters()
-        ctx.request.filters.append("gifv")
-
-        return ctx
-
-    def test_should_run_for_gif(self):
-        optimizer = Optimizer(self.get_context())
-        self.assertTrue(optimizer.should_run(".gif", ""))
-
-    def test_should_not_run_for_not_gif(self):
-        optimizer = Optimizer(self.get_context())
-        for ext in EXTENSION.items():
-            if ext != ".gif":
-                self.assertFalse(optimizer.should_run(ext, ""))
-
-    def test_should_not_run_if_binary_ffmpeg_path_does_not_exist(self):
-        self.mock_os_path_exists.return_value = False
-
-        optimizer = Optimizer(self.get_context())
-        self.assertFalse(optimizer.should_run(".gif", ""))
-
-    def test_should_parse_background_color_with_valid_value(self):
-        optimizer = Optimizer(self.get_context())
-        magenta_unicode_hex = "#ff00ff"
-        self.assertEqual(
-            optimizer.normalize_color_to_hex("ff00ff"), magenta_unicode_hex
-        )
-        self.assertEqual(
-            optimizer.normalize_color_to_hex("#ff00ff"), magenta_unicode_hex
-        )
-        self.assertEqual(
-            optimizer.normalize_color_to_hex("f0f"), magenta_unicode_hex
-        )
-        self.assertEqual(
-            optimizer.normalize_color_to_hex("#f0f"), magenta_unicode_hex
-        )
-        self.assertEqual(
-            optimizer.normalize_color_to_hex("magenta"), magenta_unicode_hex
-        )
-
-    def test_should_not_parse_background_color_with_invalid_value(self):
-        optimizer = Optimizer(self.get_context())
-        self.assertEqual(
-            optimizer.normalize_color_to_hex("asdfasdfasfd"), None
+    def test_should_be_subclass_of_thumbor_plugins(self):
+        self.assertTrue(
+            issubclass(
+                thumbor.optimizers.gifv.Optimizer,
+                thumbor_plugins.optimizers.gifv.Optimizer
+            )
         )
