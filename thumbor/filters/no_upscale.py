thumbor/thumbor

commit 44f86442ba341b9b7cccc392976e9c8191082281

fix: change if to min for height / width in no_upscale filter

thumbor/filters/no_upscale.py
@@ -27,7 +27,9 @@ class Filter(BaseFilter):
             8,
         ]:
             image_size = (image_size[1], image_size[0])
-        if self.context.request.width > image_size[0]:
-            self.context.request.width = image_size[0]
-        if self.context.request.height > image_size[1]:
-            self.context.request.height = image_size[1]
+        self.context.request.width = min(
+            self.context.request.width, image_size[0]
+        )
+        self.context.request.height = min(
+            self.context.request.height, image_size[0]
+        )

commit c68e0e9cb24e15716e437105487f9d28e8fac66b

Fix setting minimal height for no_upscale filter
It now uses the intrinsic image width iso height; this is a bug caused by https://github.com/thumbor/thumbor/commit/44f86442ba341b9b7cccc392976e9c8191082281

thumbor/filters/no_upscale.py
@@ -31,5 +31,5 @@ class Filter(BaseFilter):
             self.context.request.width, image_size[0]
         )
         self.context.request.height = min(
-            self.context.request.height, image_size[0]
+            self.context.request.height, image_size[1]
         )