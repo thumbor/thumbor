cbc084b494e616d235efd1f9889313a872fd88df
Marcelo Jorge Vieira <metal@alucinados.com>
2025-05-03 20:49:25 -0300

    Remove perf folder and configurations
    perf workflow was removed here a26e46718e8dcd632015fb003adb941300a863de

--- perf/static_app.py
+++
@@ -1,9 +0,0 @@
-from starlette.applications import Starlette
-from starlette.routing import Mount
-from starlette.staticfiles import StaticFiles
-
-ROUTES = [
-    Mount("/", app=StaticFiles(directory="static"), name="static"),
-]
-
-APP = Starlette(routes=ROUTES)
