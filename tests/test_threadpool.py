8091f47d46bb8d06090983e3db46356a36772b8a
Marcelo Jorge Vieira <metal@alucinados.com>
2026-03-12 18:30:48 -0300

    test(threadpool): replace preggy with pytest assertions

--- tests/test_threadpool.py
+++ tests/test_threadpool.py
@@ -13,7 +13,6 @@ from time import sleep
 from unittest import mock
 
 import pytest
-from preggy import expect
 
 from thumbor.threadpool import ThreadPool
 
@@ -21,92 +20,92 @@ from thumbor.threadpool import ThreadPool
 @pytest.mark.asyncio
 async def test_can_get_threadpool_instance():
     instance = ThreadPool.instance(0)
-    expect(instance.pool).to_be_null()
+    assert instance.pool is None
 
     instance = ThreadPool.instance(10)
-    expect(instance).not_to_be_null()
+    assert instance is not None
 
     instance2 = ThreadPool.instance(10)
-    expect(instance2).to_equal(instance)
+    assert instance2 == instance
 
     instance3 = ThreadPool.instance(11)
-    expect(instance3).not_to_equal(instance)
+    assert instance3 != instance
 
 
 @pytest.mark.asyncio
 async def test_can_run_task_in_foreground():
     instance = ThreadPool.instance(0)
-    expect(instance).not_to_be_null()
+    assert instance is not None
 
     def add():
         return 10
 
     result = await instance._execute_in_foreground(add)
-    expect(result).to_equal(10)
+    assert result == 10
 
 
 @pytest.mark.asyncio
 async def test_can_run_task_in_foreground_and_exception_happens():
     instance = ThreadPool.instance(0)
-    expect(instance).not_to_be_null()
+    assert instance is not None
     exception = Exception("Boom")
 
     def add():
         raise exception
 
-    with expect.error_to_happen(Exception, message="Boom"):
+    with pytest.raises(Exception, match="Boom"):
         await instance._execute_in_foreground(add)
 
 
 @pytest.mark.asyncio
 async def test_queueing_task_when_no_pool_runs_sync():
     instance = ThreadPool.instance(0)
-    expect(instance).not_to_be_null()
+    assert instance is not None
 
     def add():
         return 10
 
     result = await instance.queue(add)
-    expect(result).to_equal(10)
+    assert result == 10
 
 
 @pytest.mark.asyncio
 async def test_queueing_task_when_no_pool_runs_sync_and_exception_happens():
     instance = ThreadPool.instance(0)
-    expect(instance).not_to_be_null()
+    assert instance is not None
     exception = Exception("Boom")
 
     def add():
         raise exception
 
-    with expect.error_to_happen(Exception, message="Boom"):
+    with pytest.raises(Exception, match="Boom"):
         await instance.queue(add)
 
 
 @pytest.mark.asyncio
 async def test_can_run_async():
     instance = ThreadPool.instance(10)
-    expect(instance).not_to_be_null()
+    assert instance is not None
 
     def add():
         sleep(1)
         return 10
 
     result = await instance._execute_in_pool(add)
-    expect(result).to_equal(10)
+    assert result == 10
 
 
 @pytest.mark.asyncio
 async def test_can_run_async_with_queue():
     instance = ThreadPool.instance(10)
-    expect(instance).not_to_be_null()
+    assert instance is not None
 
     def add(value):
         sleep(1)
         return value
 
     result = await instance.queue(add, 10)
-    expect(result).to_equal(10)
+    assert result == 10
 
 
 @pytest.mark.asyncio
@@ -115,4 +114,4 @@ async def test_can_cleanup_pool():
     instance.pool = mock.Mock()
     instance.cleanup()
 
-    expect(instance.pool.shutdown.called).to_be_true()
+    assert instance.pool.shutdown.called
