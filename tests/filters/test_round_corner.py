thumbor/thumbor

commit 168065cbf20a419d7d11a87824e7ae6812ed49da

Add transparent support for filter 'round-corner': Usage: round_corner(a|b,r,g,b [,transparent]) (#1163)

thumbor/ext/filters/_round_corner.c
@@ -5,10 +5,10 @@ _round_corner_apply(PyObject *self, PyObject *args)
 {
     PyObject *buffer = NULL, *image_mode = NULL, *a_radius_py = NULL, *b_radius_py = NULL,
              *width_py = NULL, *height_py = NULL, *r_py = NULL, *b_py = NULL, *g_py = NULL,
-             *aa_py = NULL;
+             *aa_py = NULL, *transparent_py = NULL;
 
-    if (!PyArg_UnpackTuple(args, "apply", 10, 10, &aa_py, &image_mode, &a_radius_py, &b_radius_py,
-            &r_py, &g_py, &b_py, &width_py, &height_py, &buffer)) {
+    if (!PyArg_UnpackTuple(args, "apply", 10, 11, &aa_py, &image_mode, &a_radius_py, &b_radius_py,
+            &r_py, &g_py, &b_py, &width_py, &height_py, &buffer, &transparent_py)) {
         return NULL;
     }
 
@@ -25,11 +25,17 @@ _round_corner_apply(PyObject *self, PyObject *args)
                   g = (unsigned char) PyInt_AsLong(g_py),
                   b = (unsigned char) PyInt_AsLong(b_py);
 
+    char transparent = 0;
+    if (transparent_py != NULL) {
+        transparent = (char) PyObject_IsTrue(transparent_py);
+    }
+
     int num_bytes = bytes_per_pixel(image_mode_str);
 
     int r_idx = rgb_order(image_mode_str, 'R'),
         g_idx = rgb_order(image_mode_str, 'G'),
-        b_idx = rgb_order(image_mode_str, 'B');
+        b_idx = rgb_order(image_mode_str, 'B'),
+        a_idx = rgb_order(image_mode_str, 'A');
 
     float aa_amount = .75f;
 
@@ -65,23 +71,41 @@ _round_corner_apply(PyObject *self, PyObject *args)
             assert(top_left + r_idx < image_size);
             assert(top_left + g_idx < image_size);
             assert(top_left + b_idx < image_size);
+            if (transparent == 1) {
+                assert(top_left + a_idx < image_size);
+            }
             assert(bottom_left + r_idx < image_size);
             assert(bottom_left + g_idx < image_size);
             assert(bottom_left + b_idx < image_size);
+            if (transparent == 1) {
+                assert(bottom_left + a_idx < image_size);
+            }
             ptr[top_left + r_idx] = ptr[bottom_left + r_idx] = r;
             ptr[top_left + g_idx] = ptr[bottom_left + g_idx] = g;
             ptr[top_left + b_idx] = ptr[bottom_left + b_idx] = b;
+            if (transparent == 1) {
+                ptr[top_left + a_idx] = ptr[bottom_left + a_idx] = 0;
+            }
 
             if (curr_x > 0) {
                 assert(top_right + r_idx < image_size);
                 assert(top_right + g_idx < image_size);
                 assert(top_right + b_idx < image_size);
+                if (transparent == 1) {
+                    assert(top_right + a_idx < image_size);
+                }
                 assert(bottom_right + r_idx < image_size);
                 assert(bottom_right + g_idx < image_size);
                 assert(bottom_right + b_idx < image_size);
+                if (transparent == 1) {
+                    assert(bottom_right + a_idx < image_size);
+                }
                 ptr[top_right + r_idx] = ptr[bottom_right + r_idx] = r;
                 ptr[top_right + g_idx] = ptr[bottom_right + g_idx] = g;
                 ptr[top_right + b_idx] = ptr[bottom_right + b_idx] = b;
+                if (transparent == 1) {
+                    ptr[top_right + a_idx] = ptr[bottom_right + a_idx] = 0;
+                }
             }
         }
 
@@ -129,21 +153,39 @@ _round_corner_apply(PyObject *self, PyObject *args)
 
             float aa = 1.f - ((idx / (float)pixel_count_x) * aa_amount);
 
-            ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa)) + (r * (aa));
-            ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa)) + (g * (aa));
-            ptr[top_left + b_idx] = (color_top_left[b_idx] * (1.f - aa)) + (b * (aa));
+            if (transparent == 1) {
+                ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa));
+                ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa));
+                ptr[top_left + b_idx] = (color_top_left[b_idx] * (1.f - aa));
 
-            ptr[bottom_left + r_idx] = (color_bottom_left[r_idx] * (1.f - aa)) + (r * (aa));
-            ptr[bottom_left + g_idx] = (color_bottom_left[g_idx] * (1.f - aa)) + (g * (aa));
-            ptr[bottom_left + b_idx] = (color_bottom_left[b_idx] * (1.f - aa)) + (b * (aa));
+                ptr[bottom_left + r_idx] = (color_bottom_left[r_idx] * (1.f - aa));
+                ptr[bottom_left + g_idx] = (color_bottom_left[g_idx] * (1.f - aa));
+                ptr[bottom_left + b_idx] = (color_bottom_left[b_idx] * (1.f - aa));
 
-            ptr[top_right + r_idx] = (color_top_right[r_idx] * (1.f - aa)) + (r * (aa));
-            ptr[top_right + g_idx] = (color_top_right[g_idx] * (1.f - aa)) + (g * (aa));
-            ptr[top_right + b_idx] = (color_top_right[b_idx] * (1.f - aa)) + (b * (aa));
+                ptr[top_right + r_idx] = (color_top_right[r_idx] * (1.f - aa));
+                ptr[top_right + g_idx] = (color_top_right[g_idx] * (1.f - aa));
+                ptr[top_right + b_idx] = (color_top_right[b_idx] * (1.f - aa));
 
-            ptr[bottom_right + r_idx] = (color_bottom_right[r_idx] * (1.f - aa)) + (r * (aa));
-            ptr[bottom_right + g_idx] = (color_bottom_right[g_idx] * (1.f - aa)) + (g * (aa));
-            ptr[bottom_right + b_idx] = (color_bottom_right[b_idx] * (1.f - aa)) + (b * (aa));
+                ptr[bottom_right + r_idx] = (color_bottom_right[r_idx] * (1.f - aa));
+                ptr[bottom_right + g_idx] = (color_bottom_right[g_idx] * (1.f - aa));
+                ptr[bottom_right + b_idx] = (color_bottom_right[b_idx] * (1.f - aa));
+            } else {
+                ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa)) + (r * (aa));
+                ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa)) + (g * (aa));
+                ptr[top_left + b_idx] = (color_top_left[b_idx] * (1.f - aa)) + (b * (aa));
+
+                ptr[bottom_left + r_idx] = (color_bottom_left[r_idx] * (1.f - aa)) + (r * (aa));
+                ptr[bottom_left + g_idx] = (color_bottom_left[g_idx] * (1.f - aa)) + (g * (aa));
+                ptr[bottom_left + b_idx] = (color_bottom_left[b_idx] * (1.f - aa)) + (b * (aa));
+
+                ptr[top_right + r_idx] = (color_top_right[r_idx] * (1.f - aa)) + (r * (aa));
+                ptr[top_right + g_idx] = (color_top_right[g_idx] * (1.f - aa)) + (g * (aa));
+                ptr[top_right + b_idx] = (color_top_right[b_idx] * (1.f - aa)) + (b * (aa));
+
+                ptr[bottom_right + r_idx] = (color_bottom_right[r_idx] * (1.f - aa)) + (r * (aa));
+                ptr[bottom_right + g_idx] = (color_bottom_right[g_idx] * (1.f - aa)) + (g * (aa));
+                ptr[bottom_right + b_idx] = (color_bottom_right[b_idx] * (1.f - aa)) + (b * (aa));
+            }
         }
 
     }
@@ -196,21 +238,39 @@ _round_corner_apply(PyObject *self, PyObject *args)
 
             float aa = 1.f - ((idx / (float)pixel_count_y) * aa_amount);
 
-            ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa)) + (r * (aa));
-            ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa)) + (g * (aa));
-            ptr[top_left + b_idx] = (color_top_left[b_idx] * (1.f - aa)) + (b * (aa));
+            if (transparent == 1) {
+                ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa));
+                ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa));
+                ptr[top_left + b_idx] = (color_top_left[b_idx] * (1.f - aa));
 
-            ptr[bottom_left + r_idx] = (color_bottom_left[r_idx] * (1.f - aa)) + (r * (aa));
-            ptr[bottom_left + g_idx] = (color_bottom_left[g_idx] * (1.f - aa)) + (g * (aa));
-            ptr[bottom_left + b_idx] = (color_bottom_left[b_idx] * (1.f - aa)) + (b * (aa));
+                ptr[bottom_left + r_idx] = (color_bottom_left[r_idx] * (1.f - aa));
+                ptr[bottom_left + g_idx] = (color_bottom_left[g_idx] * (1.f - aa));
+                ptr[bottom_left + b_idx] = (color_bottom_left[b_idx] * (1.f - aa));
 
-            ptr[top_right + r_idx] = (color_top_right[r_idx] * (1.f - aa)) + (r * (aa));
-            ptr[top_right + g_idx] = (color_top_right[g_idx] * (1.f - aa)) + (g * (aa));
-            ptr[top_right + b_idx] = (color_top_right[b_idx] * (1.f - aa)) + (b * (aa));
+                ptr[top_right + r_idx] = (color_top_right[r_idx] * (1.f - aa));
+                ptr[top_right + g_idx] = (color_top_right[g_idx] * (1.f - aa));
+                ptr[top_right + b_idx] = (color_top_right[b_idx] * (1.f - aa));
 
-            ptr[bottom_right + r_idx] = (color_bottom_right[r_idx] * (1.f - aa)) + (r * (aa));
-            ptr[bottom_right + g_idx] = (color_bottom_right[g_idx] * (1.f - aa)) + (g * (aa));
-            ptr[bottom_right + b_idx] = (color_bottom_right[b_idx] * (1.f - aa)) + (b * (aa));
+                ptr[bottom_right + r_idx] = (color_bottom_right[r_idx] * (1.f - aa));
+                ptr[bottom_right + g_idx] = (color_bottom_right[g_idx] * (1.f - aa));
+                ptr[bottom_right + b_idx] = (color_bottom_right[b_idx] * (1.f - aa));
+            } else {
+                ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa)) + (r * (aa));
+                ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa)) + (g * (aa));
+                ptr[top_left + b_idx] = (color_top_left[b_idx] * (1.f - aa)) + (b * (aa));
+
+                ptr[bottom_left + r_idx] = (color_bottom_left[r_idx] * (1.f - aa)) + (r * (aa));
+                ptr[bottom_left + g_idx] = (color_bottom_left[g_idx] * (1.f - aa)) + (g * (aa));
+                ptr[bottom_left + b_idx] = (color_bottom_left[b_idx] * (1.f - aa)) + (b * (aa));
+
+                ptr[top_right + r_idx] = (color_top_right[r_idx] * (1.f - aa)) + (r * (aa));
+                ptr[top_right + g_idx] = (color_top_right[g_idx] * (1.f - aa)) + (g * (aa));
+                ptr[top_right + b_idx] = (color_top_right[b_idx] * (1.f - aa)) + (b * (aa));
+
+                ptr[bottom_right + r_idx] = (color_bottom_right[r_idx] * (1.f - aa)) + (r * (aa));
+                ptr[bottom_right + g_idx] = (color_bottom_right[g_idx] * (1.f - aa)) + (g * (aa));
+                ptr[bottom_right + b_idx] = (color_bottom_right[b_idx] * (1.f - aa)) + (b * (aa));
+            }
         }
     }
 

tests/filters/test_round_corner.py
@@ -1,0 +1,64 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com thumbor@googlegroups.com
+
+from preggy import expect
+
+from tests.base import FilterTestCase
+
+
+class RoundCornerFilterTestCase(FilterTestCase):
+    def test_round_corner_filter_with_a_radius(self):
+        image = self.get_filtered('source.jpg', 'thumbor.filters.round_corner', 'round_corner(50,255,0,0)')
+        expected = self.get_fixture('round_corner.jpg')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_round_corner_filter_with_ab_radius(self):
+        image = self.get_filtered('source.jpg', 'thumbor.filters.round_corner', 'round_corner(50|75,255,0,0)')
+        expected = self.get_fixture('round_corner1.jpg')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_round_corner_filter_with_blue_background(self):
+        image = self.get_filtered('source.jpg', 'thumbor.filters.round_corner', 'round_corner(50,0,0,255)')
+        expected = self.get_fixture('round_corner2.jpg')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_round_corner_filter_without_transparent(self):
+        image = self.get_filtered('source.jpg', 'thumbor.filters.round_corner', 'round_corner(50,255,0,0,0)')
+        expected = self.get_fixture('round_corner3.jpg')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_round_corner_filter_with_transparent(self):
+        image = self.get_filtered('source.jpg', 'thumbor.filters.round_corner', 'round_corner(50,255,0,0,1)')
+        expected = self.get_fixture('round_corner3.jpg')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_round_corner_filter_with_transparent_by_png(self):
+        image = self.get_filtered('source.jpg', 'thumbor.filters.round_corner', 'round_corner(50,255,0,0,1)', mode='RGBA')
+        expected = self.get_fixture('round_corner.png', mode='RGBA')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)
+
+    def test_round_corner_filter_with_transparent_by_webp(self):
+        image = self.get_filtered('source.jpg', 'thumbor.filters.round_corner', 'round_corner(50,255,0,0,1)', mode='RGBA')
+        expected = self.get_fixture('round_corner.webp', mode='RGBA')
+
+        ssim = self.get_ssim(image, expected)
+        expect(ssim).to_be_greater_than(0.99)

tests/base.py
@@ -252,11 +252,11 @@ class FilterTestCase(PythonTestCase):
     def get_fixture_path(self, name):
         return './tests/fixtures/filters/%s' % name
 
-    def get_fixture(self, name):
+    def get_fixture(self, name, mode='RGB'):
         im = Image.open(self.get_fixture_path(name))
-        return im.convert('RGB')
+        return im.convert(mode)
 
-    def get_filtered(self, source_image, filter_name, params_string, config_context=None):
+    def get_filtered(self, source_image, filter_name, params_string, config_context=None, mode='RGB'):
         fltr = self.get_filter(filter_name, params_string, config_context)
         im = Image.open(self.get_fixture_path(source_image))
         img_buffer = BytesIO()
@@ -278,7 +278,7 @@ class FilterTestCase(PythonTestCase):
 
         fltr.run(dummy_callback)
 
-        fltr.engine.image = fltr.engine.image.convert('RGB')
+        fltr.engine.image = fltr.engine.image.convert(mode)
 
         return fltr.engine.image
 

commit bc94414dfb7c98c31c84562ddcf1d080fef4a037

fix: antialias transparent rounded corners using alpha
Transparent rounded corners darkened edge RGB values instead of smoothing their
opacity, leaving a dark fringe. Apply antialiasing to the original alpha values
and preserve RGB in transparent mode.

The incorrect RGB blending was introduced in 168065cbf20a when transparent round
corners were added in 2019.

Keep an original alpha snapshot so corner clipping and both antialiasing passes
use the source opacity without making transparent pixels visible. The temporary
snapshot uses one byte per pixel.

Add regression coverage for PNG and lossless WebP output, circular and
elliptical corners, clamped radii, RGB and RGBA inputs, and spatially varying
colors and alpha.

Fixes #1687

thumbor/ext/filters/_round_corner.c
@@ -25,6 +25,19 @@ _round_corner_apply(PyObject *self, PyObject *args)
         b_idx = rgb_order(image_mode_str, 'B'),
         a_idx = rgb_order(image_mode_str, 'A');
 
+    unsigned char *original_alpha = NULL;
+    if (transparent == 1) {
+        /* Clipping and both antialiasing passes modify alpha in place. */
+        Py_ssize_t pixel_count = (Py_ssize_t)width * height;
+        original_alpha = PyMem_Malloc(pixel_count);
+        if (original_alpha == NULL) {
+            return PyErr_NoMemory();
+        }
+        for (Py_ssize_t i = 0; i < pixel_count; ++i) {
+            original_alpha[i] = ptr[i * num_bytes + a_idx];
+        }
+    }
+
     float aa_amount = .75f;
 
     if (a_radius > width / 2) {
@@ -68,11 +81,12 @@ _round_corner_apply(PyObject *self, PyObject *args)
             if (transparent == 1) {
                 assert(bottom_left + a_idx < image_size);
             }
-            ptr[top_left + r_idx] = ptr[bottom_left + r_idx] = r;
-            ptr[top_left + g_idx] = ptr[bottom_left + g_idx] = g;
-            ptr[top_left + b_idx] = ptr[bottom_left + b_idx] = b;
             if (transparent == 1) {
                 ptr[top_left + a_idx] = ptr[bottom_left + a_idx] = 0;
+            } else {
+                ptr[top_left + r_idx] = ptr[bottom_left + r_idx] = r;
+                ptr[top_left + g_idx] = ptr[bottom_left + g_idx] = g;
+                ptr[top_left + b_idx] = ptr[bottom_left + b_idx] = b;
             }
 
             if (curr_x > 0) {
@@ -88,11 +102,12 @@ _round_corner_apply(PyObject *self, PyObject *args)
                 if (transparent == 1) {
                     assert(bottom_right + a_idx < image_size);
                 }
-                ptr[top_right + r_idx] = ptr[bottom_right + r_idx] = r;
-                ptr[top_right + g_idx] = ptr[bottom_right + g_idx] = g;
-                ptr[top_right + b_idx] = ptr[bottom_right + b_idx] = b;
                 if (transparent == 1) {
                     ptr[top_right + a_idx] = ptr[bottom_right + a_idx] = 0;
+                } else {
+                    ptr[top_right + r_idx] = ptr[bottom_right + r_idx] = r;
+                    ptr[top_right + g_idx] = ptr[bottom_right + g_idx] = g;
+                    ptr[top_right + b_idx] = ptr[bottom_right + b_idx] = b;
                 }
             }
         }
@@ -142,21 +157,10 @@ _round_corner_apply(PyObject *self, PyObject *args)
             float aa = 1.f - ((idx / (float)pixel_count_x) * aa_amount);
 
             if (transparent == 1) {
-                ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa));
-                ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa));
-                ptr[top_left + b_idx] = (color_top_left[b_idx] * (1.f - aa));
-
-                ptr[bottom_left + r_idx] = (color_bottom_left[r_idx] * (1.f - aa));
-                ptr[bottom_left + g_idx] = (color_bottom_left[g_idx] * (1.f - aa));
-                ptr[bottom_left + b_idx] = (color_bottom_left[b_idx] * (1.f - aa));
-
-                ptr[top_right + r_idx] = (color_top_right[r_idx] * (1.f - aa));
-                ptr[top_right + g_idx] = (color_top_right[g_idx] * (1.f - aa));
-                ptr[top_right + b_idx] = (color_top_right[b_idx] * (1.f - aa));
-
-                ptr[bottom_right + r_idx] = (color_bottom_right[r_idx] * (1.f - aa));
-                ptr[bottom_right + g_idx] = (color_bottom_right[g_idx] * (1.f - aa));
-                ptr[bottom_right + b_idx] = (color_bottom_right[b_idx] * (1.f - aa));
+                ptr[top_left + a_idx] = original_alpha[top_left / num_bytes] * (1.f - aa);
+                ptr[bottom_left + a_idx] = original_alpha[bottom_left / num_bytes] * (1.f - aa);
+                ptr[top_right + a_idx] = original_alpha[top_right / num_bytes] * (1.f - aa);
+                ptr[bottom_right + a_idx] = original_alpha[bottom_right / num_bytes] * (1.f - aa);
             } else {
                 ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa)) + (r * (aa));
                 ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa)) + (g * (aa));
@@ -227,21 +231,10 @@ _round_corner_apply(PyObject *self, PyObject *args)
             float aa = 1.f - ((idx / (float)pixel_count_y) * aa_amount);
 
             if (transparent == 1) {
-                ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa));
-                ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa));
-                ptr[top_left + b_idx] = (color_top_left[b_idx] * (1.f - aa));
-
-                ptr[bottom_left + r_idx] = (color_bottom_left[r_idx] * (1.f - aa));
-                ptr[bottom_left + g_idx] = (color_bottom_left[g_idx] * (1.f - aa));
-                ptr[bottom_left + b_idx] = (color_bottom_left[b_idx] * (1.f - aa));
-
-                ptr[top_right + r_idx] = (color_top_right[r_idx] * (1.f - aa));
-                ptr[top_right + g_idx] = (color_top_right[g_idx] * (1.f - aa));
-                ptr[top_right + b_idx] = (color_top_right[b_idx] * (1.f - aa));
-
-                ptr[bottom_right + r_idx] = (color_bottom_right[r_idx] * (1.f - aa));
-                ptr[bottom_right + g_idx] = (color_bottom_right[g_idx] * (1.f - aa));
-                ptr[bottom_right + b_idx] = (color_bottom_right[b_idx] * (1.f - aa));
+                ptr[top_left + a_idx] = original_alpha[top_left / num_bytes] * (1.f - aa);
+                ptr[bottom_left + a_idx] = original_alpha[bottom_left / num_bytes] * (1.f - aa);
+                ptr[top_right + a_idx] = original_alpha[top_right / num_bytes] * (1.f - aa);
+                ptr[bottom_right + a_idx] = original_alpha[bottom_right / num_bytes] * (1.f - aa);
             } else {
                 ptr[top_left + r_idx] = (color_top_left[r_idx] * (1.f - aa)) + (r * (aa));
                 ptr[top_left + g_idx] = (color_top_left[g_idx] * (1.f - aa)) + (g * (aa));
@@ -264,6 +257,7 @@ _round_corner_apply(PyObject *self, PyObject *args)
 
 END:
 
+    PyMem_Free(original_alpha);
     Py_INCREF(buffer);
     return buffer;
 }

tests/filters/test_round_corner.py
@@ -7,9 +7,84 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
+from io import BytesIO
+
+import pytest
+from PIL import Image, ImageChops
 from tornado.testing import gen_test
 
 from tests.base import FilterTestCase
+
+
+async def round_transparent_image(source, radius, extension=".png"):
+    fltr = FilterTestCase().get_filter(
+        "thumbor.filters.round_corner", f"round_corner({radius},0,0,0,1)"
+    )
+    with BytesIO() as buffer:
+        source.save(buffer, "PNG")
+        fltr.engine.load(buffer.getvalue(), ".png")
+    await fltr.run()
+    with Image.open(
+        BytesIO(fltr.engine.read(extension, quality=100))
+    ) as result:
+        return result.convert("RGBA")
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("radius", ["30", "20|35", "150"])
+@pytest.mark.parametrize("mode, alpha", [("RGB", 255), ("RGBA", 128)])
+@pytest.mark.parametrize("extension", [".png", ".webp"])
+async def test_transparent_round_corners_have_no_dark_border(
+    radius, mode, alpha, extension
+):
+    source = Image.new(mode, (100, 100), "white")
+    if mode == "RGBA":
+        source.putalpha(alpha)
+    result = await round_transparent_image(source, radius, extension)
+
+    background = Image.new("RGBA", source.size, "white")
+    composite = Image.alpha_composite(background, result).convert("RGB")
+    assert composite.getextrema() == ((255, 255),) * 3
+    assert result.getpixel((50, 50)) == (255, 255, 255, alpha)
+
+    for box in (
+        (0, 0, 50, 50),
+        (50, 0, 100, 50),
+        (0, 50, 50, 100),
+        (50, 50, 100, 100),
+    ):
+        histogram = result.getchannel("A").crop(box).histogram()
+        assert histogram[0] > 0
+        assert histogram[alpha] > 0
+        assert sum(histogram[1:alpha]) > 0
+        assert sum(histogram[alpha + 1 :]) == 0
+
+
+@pytest.mark.asyncio
+async def test_transparent_round_corners_preserve_existing_color_and_alpha():
+    source = Image.new("RGBA", (100, 100))
+    source.putdata(
+        [
+            (2 * x, 2 * y, 150, ((x + y) % 3) * 127)
+            for y in range(100)
+            for x in range(100)
+        ]
+    )
+    result = await round_transparent_image(source, "20|35")
+
+    assert (
+        ImageChops.difference(
+            result.convert("RGB"), source.convert("RGB")
+        ).getbbox()
+        is None
+    )
+    assert (
+        ImageChops.subtract(
+            result.getchannel("A"), source.getchannel("A")
+        ).getbbox()
+        is None
+    )
+    assert result.getpixel((50, 50)) == source.getpixel((50, 50))
 
 
 class RoundCornerFilterTestCase(FilterTestCase):